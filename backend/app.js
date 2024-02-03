58fdf4eb15c6312a8e30020210f47fd79d904bb3
SergioMEV <sergiomarteloe@gmail.com>
2024-02-03 17:57:25 -0600

    doc: adds comments to app.js

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Routes 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Starts app
 var app = express();
 
+// App configurations
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Sets routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
