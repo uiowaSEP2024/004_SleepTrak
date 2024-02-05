80c132346cb6ec34c9e7752f02756637c3aa0387
Haruko Okada <hokada@uiowa.edu>
2024-02-04 22:08:16 -0600

    set up: added comments to metro.config.js

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,13 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
+// Find the project and workspace directories
 const workspaceRoot = path.resolve(__dirname, '../');
 const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
 
+// 1. Watch all files within the monorepo
 config.watchFolders = [workspaceRoot];
 
+// 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
 path.resolve(projectRoot, 'node_modules'),
 path.resolve(workspaceRoot, 'node_modules'),
@@ -15,4 +18,5 @@ path.resolve(workspaceRoot, 'node_modules'),
 
 module.exports = config;
 
+// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
