e95b1c769e4ddf4c76ae7bc3a7deae64136111ef
SergioMEV <sergiomarteloe@gmail.com>
2024-02-05 21:11:35 -0600

    style: formats route files with prettier

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,61 +1,57 @@
 var express = require('express');
 var router = express.Router();
-var { prisma } = require("../prisma/client");
+var { prisma } = require('../prisma/client');
 
 router.get('/all', async (req, res) => {
-  const users = await prisma.event.findMany()
-  res.json(users)
+  const users = await prisma.event.findMany();
+  res.json(users);
 });
 
 router.get('/search', async (req, res) => {
   const users = await prisma.event.findMany({
     where: req.body
-  })
-  res.json(users)
+  });
+  res.json(users);
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
 
-  const user = await prisma.event
-    .findUnique({
-      where: {
-        eventId: id,
-      },
-    });
+  const user = await prisma.event.findUnique({
+    where: {
+      eventId: id
+    }
+  });
   res.json(user);
-})
+});
 
 router.post('/create', async (req, res) => {
   const eventParams = req.body;
   const result = await prisma.event.create(eventParams);
-  res.json(result)
-})
-
+  res.json(result);
+});
 
 router.put('/:id/update', async (req, res) => {
   const { id } = req.params;
   const valuesToUpdate = req.body;
-  const result = await prisma.user
-    .update({
-      where: {
-        eventId: id
-      },
-      data: valuesToUpdate
-    });
+  const result = await prisma.user.update({
+    where: {
+      eventId: id
+    },
+    data: valuesToUpdate
+  });
 
   res.json(result);
-})
+});
 
-router.delete("/:id/delete", async (req, res) => {
+router.delete('/:id/delete', async (req, res) => {
   const { id } = req.params;
   const result = await prisma.event.delete({
     where: {
-      eventId: id,
+      eventId: id
     }
   });
   res.json(result);
-})
+});
 
 module.exports = router;
-
