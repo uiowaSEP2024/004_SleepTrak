4c322810d3892288e3652806bbb6b64f7e7b38bb
SergioMEV <sergiomarteloe@gmail.com>
2024-02-06 19:59:44 -0600

    style: changes app folder to src

---
+++ backend/src/controllers/utils.js
@@ -0,0 +1,17 @@
+function collapseObject(obj) {
+  const result = {};
+  for (const key in obj) {
+    if (obj[key] !== null) {
+      if (typeof obj[key] === 'object') {
+        result[key] = collapseObject(obj[key]);
+      } else {
+        result[key] = obj[key];
+      }
+    }
+  }
+  return result;
+}
+
+module.exports = {
+  collapseObject
+};
---
+++ backend/src/services/plans.js
@@ -0,0 +1,74 @@
+var { prisma } = require('../../prisma/client');
+
+module.exports = {
+  getAll: async (planId) => {
+    try {
+      const result = await prisma.plan.findMany();
+
+      return result;
+    } catch (err) {
+      return err;
+    }
+  },
+  get: async (planId) => {
+    console.log(planId);
+    try {
+      const result = await prisma.plan.findUnique({
+        where: {
+          planId: planId
+        }
+      });
+
+      return result;
+    } catch (err) {
+      return err;
+    }
+  },
+  search: async (searchParams) => {
+    try {
+      const result = await prisma.plan.findMany({
+        where: searchParams
+      });
+
+      return result;
+    } catch (err) {
+      return err;
+    }
+  },
+  create: async (planData) => {
+    try {
+      const result = await prisma.plan.create({ data: planData });
+
+      return result;
+    } catch (err) {
+      return err;
+    }
+  },
+  update: async (planId, valuesToUpdate) => {
+    try {
+      const result = await prisma.user.update({
+        where: {
+          planId: planId
+        },
+        data: valuesToUpdate
+      });
+
+      return result;
+    } catch (err) {
+      return err;
+    }
+  },
+  destroy: async (planId) => {
+    try {
+      const result = await prisma.plan.delete({
+        where: {
+          planId: planId
+        }
+      });
+
+      return result;
+    } catch (err) {
+      return err;
+    }
+  }
+};
