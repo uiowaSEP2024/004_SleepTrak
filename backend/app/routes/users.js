047a03701f6b266e152e8c6a19cedac5a84a05aa
SergioMEV <sergiomarteloe@gmail.com>
2024-02-06 19:52:46 -0600

    style: moves folders around

---
+++ backend/app/routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/users');
+
+router.get('/all', controller.getAll);
+router.get('/search', controller.search);
+router.get('/:id', controller.get);
+router.post('/create', controller.create);
+router.put('/:id/update', controller.update);
+router.delete('/:id/delete', controller.destroy);
+
+module.exports = router;
