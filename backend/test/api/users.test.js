f23fdcf282ef6f5f1e88485c4386a33bcb2093cc
dlaurianti <dlaurianti@uiowa.edu>
2024-02-08 14:43:53 -0600

    test: tests for search, create, update users routes

--- backend/test/api/users.test.js
+++ backend/test/api/users.test.js
@@ -2,37 +2,44 @@ const request = require('supertest');
 const app = require('../../src/app');
 const { prismaMock } = require('../mock_client');
 
+const mockUsers = [
+  {
+    id: 1,
+    first_name: 'Dylan',
+    last_name: 'Laurianti',
+    email: 'dl@email.js',
+    role: 'owner'
+  },
+  {
+    id: 2,
+    first_name: 'Sergio',
+    last_name: 'Martelo',
+    email: 'sm@email.js',
+    role: 'client'
+  },
+  {
+    id: 3,
+    first_name: 'Haruko',
+    last_name: 'Okada',
+    email: 'ho@email.js',
+    role: 'coach'
+  },
+  {
+    id: 4,
+    first_name: 'Mingi',
+    last_name: 'Lee',
+    email: 'ml@email.js',
+    role: 'client'
+  }
+];
+const user = {
+  first_name: 'Test',
+  last_name: 'User',
+  email: 'test@email.js',
+  role: 'coach'
+};
+
 describe('Test /users/all route', () => {
-  const mockUsers = [
-    {
-      id: 1,
-      first_name: 'Dylan',
-      last_name: 'Laurianti',
-      email: 'dl@email.js',
-      role: 'owner'
-    },
-    {
-      id: 2,
-      first_name: 'Sergio',
-      last_name: 'Martelo',
-      email: 'sm@email.js',
-      role: 'client'
-    },
-    {
-      id: 3,
-      first_name: 'Haruko',
-      last_name: 'Okada',
-      email: 'ho@email.js',
-      role: 'coach'
-    },
-    {
-      id: 4,
-      first_name: 'Mingi',
-      last_name: 'Lee',
-      email: 'ml@email.js',
-      role: 'client'
-    }
-  ];
   test('GET /users returns all users', async () => {
     prismaMock.user.findMany.mockReturnValue(mockUsers);
     const response = await request(app).get('/users/all');
@@ -40,15 +47,56 @@ describe('Test /users/all route', () => {
     expect(response.body).toEqual(mockUsers);
     expect(prismaMock.user.findMany).toHaveBeenCalledWith();
   });
-  test('GET /users/:id returns user with id :id', async () => {
+});
+
+describe('test /users/:id route', () => {
+  test('GET /users/:id calls findUnique and returns user', async () => {
     const tid = 3;
-    prismaMock.user.findUnique.mockReturnValue(mockUsers[tid]);
+    prismaMock.user.findUnique.mockReturnValue(
+      mockUsers.filter((users) => users.id === tid)
+    );
     const response = await request(app).get('/users/{tid}');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(mockUsers[tid]);
-    expect(response.body.id);
+    expect(response.body).toEqual(
+      mockUsers.filter((users) => users.id === tid)
+    );
+    response.body.forEach((user) => expect(user.id).toEqual(tid));
     expect(prismaMock.user.findUnique).toHaveBeenCalledWith({
       where: { userId: '{tid}' }
     });
+  });
+});
+
+describe('test /users/search route', () => {
+  test('GET /users/search calls findMany and returns users', async () => {
+    const role = 'client';
+    prismaMock.user.findMany.mockReturnValue(
+      mockUsers.filter((user) => user.role === role)
+    );
+    const response = await request(app).get('/users/search');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(
+      mockUsers.filter((user) => user.role === role)
+    );
+    response.body.forEach((user) => expect(user.role).toEqual(role));
+    expect(prismaMock.user.findMany).toHaveBeenCalledWith({ where: {} });
+  });
+});
+
+describe('test /users/create route', () => {
+  test('POST /users/create calls create and returns user', async () => {
+    prismaMock.user.create.mockReturnValue(user);
+    const response = await request(app).post('/users/create');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(user);
+  });
+});
+
+describe('test /users/:id/update route', () => {
+  test('PUT /users/:id/update calls update and returns user', async () => {
+    prismaMock.user.update.mockReturnValue(user);
+    const response = await request(app).put('/users/4/update');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(user);
   });
 });
