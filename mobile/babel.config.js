ce74d983f4ccbdacee0c10825e1d7397dfeb7d60
Haruko Okada <hokada@uiowa.edu>
2024-02-11 16:33:48 -0600

    docs: wrote documentation for the plugin in babel.config.js

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // Plugin to only import modules that are used
     env: {
       production: {
         plugins: ['react-native-paper/babel']
