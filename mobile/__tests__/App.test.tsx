b3f9da01dcadacdfb9b7887f69f35db11c2b5fcb
Haruko Okada <hokada@uiowa.edu>
2024-02-15 17:29:37 -0600

    test: added first test for app.tsx

--- mobile/__tests__/App.test.tsx
+++ mobile/__tests__/App.test.tsx
@@ -1,3 +1,9 @@
-test('demo', () => {
-  expect(true).toBe(true);
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import App from '../App';
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
 });
