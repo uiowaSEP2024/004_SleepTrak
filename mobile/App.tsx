64afd596831cd0f9db138651430655487404e055
Haruko Okada <hokada@uiowa.edu>
2024-02-15 00:04:34 -0600

    feat: changed App.tsx to render nav bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabs from './src/navigation/MainNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Sleep Tracker</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <BottomTabs />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
