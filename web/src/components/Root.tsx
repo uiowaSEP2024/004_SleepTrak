bb84060efc6f7cb7676963d48015c213f0c66522
Mingi Lee <mingilee98@gmail.com>
2024-02-17 19:23:13 -0600

    fix: fix a bug where sidebar is not fixed when scrolling

--- web/src/components/Root.tsx
+++ web/src/components/Root.tsx
@@ -22,12 +22,7 @@ export default function Root() {
               md: 3
             },
             pb: { xs: 2, sm: 2, md: 3 },
-            flex: 1,
-            display: 'flex',
-            flexDirection: 'column',
-            minWidth: 0,
-            height: '100dvh',
-            gap: 1
+            flex: 1
           }}>
           <Outlet />
         </Box>
