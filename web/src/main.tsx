b54a5e37e29ba1d8513d3e7fd295082c3bebfed6
SergioMEV <sergiomarteloe@gmail.com>
2024-02-18 17:57:45 -0600

    feat: adds necessary elements to main.tsx and packages to package.json

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 
+const domain: string = import.meta.env.VITE_AUTH0_DOMAIN!;
+const clientId: string = import.meta.env.VITE_AUTH0_CLIENT_ID!;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
