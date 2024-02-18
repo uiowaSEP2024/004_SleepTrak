8c1b54fcc3bd68ce48e6ee2d4618d6016222628a
Mingi Lee <mingilee98@gmail.com>
2024-02-18 12:36:33 -0600

    feat: create client card component and page ui

---
+++ web/src/components/ClientCard.tsx
@@ -0,0 +1,50 @@
+import Avatar from '@mui/joy/Avatar';
+import Button from '@mui/joy/Button';
+import ButtonGroup from '@mui/joy/ButtonGroup';
+import Card from '@mui/joy/Card';
+import CardContent from '@mui/joy/CardContent';
+import CardOverflow from '@mui/joy/CardOverflow';
+import CardActions from '@mui/joy/CardActions';
+import Typography from '@mui/joy/Typography';
+
+interface ClientCardProps {
+  avatarSrc: string;
+  title: string;
+  body: string;
+}
+
+const ClientCard: React.FC<ClientCardProps> = ({ avatarSrc, title, body }) => {
+  return (
+    <Card
+      sx={{
+        width: 250,
+        maxWidth: '100%',
+        boxShadow: 'lg'
+      }}>
+      <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+        <Avatar
+          src={avatarSrc}
+          sx={{ '--Avatar-size': '4rem' }}
+        />
+        <Typography level="title-lg">{title}</Typography>
+        <Typography
+          level="body-sm"
+          sx={{ maxWidth: '24ch' }}>
+          {body}
+        </Typography>
+      </CardContent>
+      <CardOverflow sx={{ bgcolor: 'background.level1' }}>
+        <CardActions buttonFlex="1">
+          <ButtonGroup
+            variant="outlined"
+            sx={{ bgcolor: 'background.surface' }}>
+            <Button>Message</Button>
+            <Button>More</Button>
+          </ButtonGroup>
+        </CardActions>
+      </CardOverflow>
+    </Card>
+  );
+};
+
+export default ClientCard;
--- web/src/pages/ClientsPage.tsx
+++ web/src/pages/ClientsPage.tsx
@@ -1,5 +1,27 @@
+import ClientCard from '../components/ClientCard';
+import Grid from '@mui/joy/Grid';
+import Item from '@mui/joy/Grid';
+
 function ClientsPage() {
-  return <h1>clients</h1>;
+  return (
+    <>
+      <h1>Clients</h1>
+      <Grid
+        container
+        spacing={3}
+        sx={{ flexGrow: 1 }}>
+        <Grid xs="auto">
+          <Item>
+            <ClientCard
+              avatarSrc="/static/images/avatar/1.jpg"
+              title="Mingi Lee"
+              body="Mingi Information"
+            />
+          </Item>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default ClientsPage;
