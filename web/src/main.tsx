04a71c76739d8cf88e914b88409adb868a4669c5
SergioMEV <sergiomarteloe@gmail.com>
2024-02-18 18:28:03 -0600

    feat: protects routes and updates react-dom-routes to current implementation

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 
-const domain: string = import.meta.env.VITE_AUTH0_DOMAIN!;
-const clientId: string = import.meta.env.VITE_AUTH0_CLIENT_ID!;
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Auth0Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
