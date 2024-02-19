86cc7840ca941f344ed8a6573ce9b0a27baa092d
SergioMEV <sergiomarteloe@gmail.com>
2024-02-18 18:28:03 -0600

    feat: protects routes and updates react-dom-routes to current implementation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,40 +1,60 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ErrorPage from './pages/ErrorPage';
 import DashboardPage from './pages/DashboardPage';
 import Root from './components/Root';
 import ClientsPage from './pages/ClientsPage';
 import BabiesPage from './pages/Babies';
 import MessagesPage from './pages/Messages';
+import LoginPage from './pages/Login';
+import {
+  Auth0ProviderWithRedirectCallback,
+  ProtectedRoute
+} from './components/auth';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/dashboard',
-        element: <DashboardPage />
-      },
-      {
-        path: '/clients',
-        element: <ClientsPage />
-      },
-      {
-        path: '/babies',
-        element: <BabiesPage />
-      },
-      {
-        path: '/messages',
-        element: <MessagesPage />
-      }
-    ]
-  }
-]);
+const domain: string = import.meta.env.VITE_AUTH0_DOMAIN!;
+const clientId: string = import.meta.env.VITE_AUTH0_CLIENT_ID!;
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Auth0ProviderWithRedirectCallback
+        domain={domain}
+        clientId={clientId}
+        redirectUri={window.location.origin}>
+        <Routes>
+          <Route
+            path="/login"
+            element={<LoginPage />}
+          />
+          <Route
+            path="*"
+            element={<ErrorPage />}
+          />
+          <Route
+            path="/"
+            element={<ProtectedRoute component={Root} />}>
+            <Route
+              path="dashboard"
+              element={<DashboardPage />}
+            />
+            <Route
+              path="clients"
+              element={<ClientsPage />}
+            />
+            <Route
+              path="babies"
+              element={<BabiesPage />}
+            />
+            <Route
+              path="messages"
+              element={<MessagesPage />}
+            />
+          </Route>
+        </Routes>
+      </Auth0ProviderWithRedirectCallback>
+    </Router>
+  );
 }
 
 export default App;
