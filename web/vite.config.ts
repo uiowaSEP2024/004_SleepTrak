85f3d2c9b3d8bdd4cf8b46f40faf6ed101820945
dlaurianti <dlaurianti@uiowa.edu>
2024-02-19 14:04:23 -0600

    chore: web container working

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+      ignored: ['./vite.config.ts']
+    },
+    hmr: { host: '0.0.0.0' }
+  }
+});
