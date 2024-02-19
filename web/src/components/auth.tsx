95a620372028d6fd978a93b20a6b22ce07cf236a
SergioMEV <sergiomarteloe@gmail.com>
2024-02-18 18:26:54 -0600

    feat: adds new auth components for protecting routes

--- web/src/components/auth.tsx
+++ web/src/components/auth.tsx
@@ -1,7 +1,23 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useAuth0 } from '@auth0/auth0-react';
+import {
+  Auth0Provider,
+  WithAuthenticationRequiredOptions,
+  useAuth0,
+  withAuthenticationRequired
+} from '@auth0/auth0-react';
 import { IconButton, Button } from '@mui/joy';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import { useNavigate } from 'react-router-dom';
+import { ReactNode } from 'react';
+interface Auth0ProviderProps {
+  children: ReactNode;
+  domain: string;
+  clientId: string;
+  redirectUri?: string;
+}
+interface AppState {
+  returnTo?: string;
+}
 
 export const LogoutButton: React.FC = () => {
   const { isAuthenticated, logout } = useAuth0();
@@ -30,4 +46,32 @@ export const LoginButton: React.FC = () => {
   const { loginWithRedirect } = useAuth0();
 
   return <Button onClick={() => loginWithRedirect()}>Log In</Button>;
+};
+
+interface ProtectedRouteProps extends WithAuthenticationRequiredOptions {
+  component: React.ComponentType;
+}
+
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  component,
+  ...args
+}) => {
+  const Component = withAuthenticationRequired(component, args);
+  return <Component />;
+};
+
+export const Auth0ProviderWithRedirectCallback: React.FC<
+  Auth0ProviderProps
+> = ({ children, ...props }) => {
+  const navigate = useNavigate();
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || window.location.pathname);
+  };
+  return (
+    <Auth0Provider
+      onRedirectCallback={onRedirectCallback}
+      {...props}>
+      {children}
+    </Auth0Provider>
+  );
 };
