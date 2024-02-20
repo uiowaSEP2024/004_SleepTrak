74318d7b7350743c5391b57efa74a1b13444455b
Haruko Okada <hokada@uiowa.edu>
2024-02-19 23:57:23 -0600

    feat: added elapsed time display to timer screen

--- mobile/src/screens/SleepTimerScreen.tsx
+++ mobile/src/screens/SleepTimerScreen.tsx
@@ -2,30 +2,35 @@ import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import TimerButton from '../components/buttons/TimerButton';
 import TimerDisplay from '../components/views/TimerDisplay';
+import ElapsedTimeDisplay from '../components/views/ElapsedTimeDisplay';
 
 const SleepTimer: React.FC = () => {
   const [startTime, setStartTime] = useState<Date | null>(null);
   const [stopTime, setStopTime] = useState<Date | null>(null);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
 
   const handleStart = () => {
     setStartTime(new Date());
     setStopTime(null);
+    setIsRunning(true);
   };
 
   const handleStop = () => {
     setStopTime(new Date());
+    setIsRunning(false);
   };
 
   return (
     <View style={styles.container}>
       <TimerDisplay
         title="Start Time:"
-        time={startTime?.toLocaleTimeString()}
+        time={startTime ? startTime.toLocaleTimeString() : ''}
       />
       <TimerDisplay
         title="Stop Time:"
-        time={stopTime?.toLocaleTimeString()}
+        time={stopTime ? stopTime.toLocaleTimeString() : ''}
       />
+      <ElapsedTimeDisplay isRunning={isRunning} />
       <TimerButton
         onStart={handleStart}
         onStop={handleStop}
