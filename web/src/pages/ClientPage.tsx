819b0fadccc4916c54b8524eea57c23f0fb87b57
Mingi Lee <mingilee98@gmail.com>
2024-02-20 20:06:08 -0600

    feat: make tab in client page render pages based on selected tab

--- web/src/pages/ClientPage.tsx
+++ web/src/pages/ClientPage.tsx
@@ -4,14 +4,12 @@ import Box from '@mui/system/Box';
 
 export default function ClientPage() {
   return (
-    <>
+    <Box>
       <h2>Client 1</h2>
       <Box display="flex">
         <BabyDropdown />
       </Box>
-      <Box display="flex">
-        <BabyTab />
-      </Box>
-    </>
+      <BabyTab />
+    </Box>
   );
 }
