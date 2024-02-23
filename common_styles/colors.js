b2fda0d04029a51094fe4f49bfee1c84e03d21a1
Haruko Okada <hokada@uiowa.edu>
2024-02-23 14:13:24 -0600

    style: created color palette

---
+++ common_styles/colors.js
@@ -0,0 +1,15 @@
+/**
+ * This file contains the color palette for the app.
+ */
+
+export const colors = {
+  deepBlue: '#025977',
+  skyBlue: '#4FC3CA',
+  softBlue: '#C0CDE9',
+  silverGrey: '#9B9A9A',
+  crimsonRed: '#D75447',
+  peachyOrange: '#F89D6C',
+  lightTan: '#F5D6A8',
+  softLavender: '#D5C1D6',
+  palePink: '#F5E0ED'
+};
