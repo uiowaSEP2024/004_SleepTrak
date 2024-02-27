b5eea398bd7c80f932b71f999b9d9ba146a3c293
Mingi Lee <mingilee98@gmail.com>
2024-02-27 00:34:08 -0600

    feat: create clients index page

--- web/src/pages/ClientsPage.tsx
+++ web/src/pages/ClientsPage.tsx
@@ -1,24 +1,71 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import ClientCard from '../components/ClientCard';
 import Grid from '@mui/joy/Grid';
 import Item from '@mui/joy/Grid';
+import { useEffect, useState } from 'react';
+
+interface Baby {
+  name: string;
+  babyId: string;
+}
+
+interface User {
+  userId: string;
+  first_name: string;
+  last_name: string;
+  role: string;
+  Babies: Baby[];
+}
 
 function ClientsPage() {
+  const [usersData, setUsersData] = useState<User[]>([]);
+  const { getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch('http://localhost:3000/users/all', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      const data = await response.json();
+      setUsersData(data);
+    };
+
+    fetchData();
+  }, [getAccessTokenSilently]);
+
+  // TODO: filter so that the the page only shows current user's clients
   return (
     <>
-      <h1>Clients</h1>
+      <h2>Clients</h2>
       <Grid
         container
         spacing={3}
         sx={{ flexGrow: 1 }}>
-        <Grid xs="auto">
-          <Item>
-            <ClientCard
-              avatarSrc="/static/images/avatar/1.jpg"
-              title="Client 1"
-              body="Baby 1 (1y 2m), Baby2 (0y 2m)"
-            />
-          </Item>
-        </Grid>
+        {usersData
+          .filter((object) => object.role == 'client')
+          .map((object, index) => (
+            <Grid
+              xs="auto"
+              key={index}>
+              <Item>
+                <ClientCard
+                  avatarSrc="testAvatar"
+                  title={object.first_name + ' ' + object.last_name}
+                  body={object.Babies.map((baby) => baby.name).join(' ')}
+                  babyId={
+                    object.Babies.length > 0
+                      ? object.Babies[0].babyId || ''
+                      : ''
+                  }
+                />
+              </Item>
+            </Grid>
+          ))}
       </Grid>
     </>
   );
