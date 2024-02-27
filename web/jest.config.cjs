fe07bbfe6a251852f8a686c691e6fec23486f3fb
Haruko Okada <hokada@uiowa.edu>
2024-02-26 22:52:09 -0600

    chore: add code coverage report to web

--- web/jest.config.cjs
+++ web/jest.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: ['src/components/**/*.{ts,tsx}']
 };
