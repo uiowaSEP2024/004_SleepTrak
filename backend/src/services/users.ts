1849329b3fa22729704d44daccec5f0d244b2cbd
Mingi Lee <mingilee98@gmail.com>
2024-02-27 20:43:26 -0600

    chore: clean up unnecessary files after rebasing

--- backend/src/services/users.ts
+++ backend/src/services/users.ts
@@ -4,7 +4,11 @@ import { ensureError } from '../utils/error.js';
 
 const getAll = async (): Promise<User[] | Error> => {
   try {
-    const result = await prisma.user.findMany();
+    const result = await prisma.user.findMany({
+      include: {
+        Babies: true
+      }
+    });
 
     return result;
   } catch (err) {
@@ -16,6 +20,9 @@ const get = async (userId: string): Promise<User | null | Error> => {
     const result = await prisma.user.findUnique({
       where: {
         userId
+      },
+      include: {
+        Babies: true
       }
     });
 
