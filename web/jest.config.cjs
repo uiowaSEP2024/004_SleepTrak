111e14831d1670ca4ab4de27b7a0c7ed8988b3e0
Mingi Lee <mingilee98@gmail.com>
2024-02-27 20:30:55 -0600

    fix: change default jest test environment to jsdom

--- web/jest.config.cjs
+++ web/jest.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   collectCoverage: true,
   coverageReporters: ['json', 'lcov', 'text', 'clover'],
   collectCoverageFrom: ['src/components/**/*.{ts,tsx}'],
