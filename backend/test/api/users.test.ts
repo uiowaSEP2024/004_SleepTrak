608402f9d4b1f96461b3fdbeeef2e4fc7a74d901
dlaurianti <dlaurianti@uiowa.edu>
2024-02-28 14:28:10 -0600

    test: users tests using new test tools

--- backend/test/api/users.test.ts
+++ backend/test/api/users.test.ts
@@ -1,126 +1,139 @@
-import request from 'supertest';
-import app from '../../src/app';
-import { prismaMock } from '../mock_client.js';
+import { testRoute } from './common.js';
+import type { User } from '@prisma/client';
 
-const mockUsers = [
+const mockUsers: User[] = [
   {
-    userId: 1,
+    userId: '1',
     first_name: 'Dylan',
     last_name: 'Laurianti',
-    email: 'dl@email.js',
-    role: 'owner'
-  },
-  {
-    userId: 2,
-    first_name: 'Sergio',
-    last_name: 'Martelo',
-    email: 'sm@email.js',
-    role: 'client'
-  },
-  {
-    userId: 3,
-    first_name: 'Haruko',
-    last_name: 'Okada',
-    email: 'ho@email.js',
+    email: 'dlaurianti@uiowa.edu',
+    coachId: null,
     role: 'coach'
   },
   {
-    userId: 4,
-    first_name: 'Mingi',
-    last_name: 'Lee',
-    email: 'ml@email.js',
+    userId: '2',
+    first_name: 'Sergio',
+    last_name: 'Martelo',
+    email: 'smartelo@uiowa.edu',
+    coachId: '1',
     role: 'client'
   }
 ];
 const user = {
-  first_name: 'Test',
-  last_name: 'User',
-  email: 'test@email.js',
-  role: 'coach'
+  userId: '3',
+  first_name: 'Haruko',
+  last_name: 'Okada',
+  email: 'hokada@uiowa.edu',
+  coachId: '1',
+  role: 'client'
 };
 
-describe('Test /users/all route', () => {
-  test('GET /users returns all users', async () => {
-    prismaMock.user.findMany.mockReturnValue(mockUsers);
-    const response = await request(app).get('/users/all');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(mockUsers);
-    expect(prismaMock.user.findMany).toHaveBeenCalledWith();
-  });
+// /users/all
+testRoute({
+  reqData: {},
+  mockData: mockUsers,
+  expectData: {
+    status: 200,
+    body: mockUsers
+  },
+  model: 'user',
+  route: 'all'
 });
 
-describe('test /users/:id route', () => {
-  test('GET /users/:id calls findUnique and returns user', async () => {
-    const tid = 3;
-    prismaMock.user.findUnique.mockReturnValue(
-      mockUsers.filter((users) => users.userId === tid)
-    );
-    const response = await request(app).get('/users/{tid}');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(
-      mockUsers.filter((users) => users.userId === tid)
-    );
-    response.body.forEach((user: any) => {
-      expect(user.userId).toEqual(tid);
-    });
-    expect(prismaMock.user.findUnique).toHaveBeenCalledWith({
-      where: { userId: '{tid}' }
-    });
-  });
+// /users/:id
+testRoute({
+  reqData: {},
+  mockData: mockUsers.filter((user) => user.userId === '1'),
+  expectData: {
+    status: 200,
+    body: mockUsers.filter((user) => user.userId === '1'),
+    calledWith: {
+      where: {
+        userId: ':id'
+      }
+    }
+  },
+  model: 'user',
+  id: '1'
 });
 
-describe('test /users/search route', () => {
-  test('GET /users/search calls findMany and returns users', async () => {
-    const role = 'client';
-    prismaMock.user.findMany.mockReturnValue(
-      mockUsers.filter((user) => user.role === role)
-    );
-    const response = await request(app).get('/users/search');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(
-      mockUsers.filter((user) => user.role === role)
-    );
-    response.body.forEach((user: any) => {
-      expect(user.role).toEqual(role);
-    });
-    expect(prismaMock.user.findMany).toHaveBeenCalledWith({ where: {} });
-  });
+// /users/search
+testRoute({
+  reqData: {
+    role: 'client'
+  },
+  mockData: mockUsers.filter((user) => user.role === 'client'),
+  expectData: {
+    status: 200,
+    body: mockUsers.filter((user) => user.role === 'client'),
+    calledWith: {
+      where: {
+        role: 'client'
+      }
+    }
+  },
+  model: 'user',
+  route: 'search'
 });
 
-describe('test /users/create route', () => {
-  test('POST /users/create calls create and returns user', async () => {
-    prismaMock.user.create.mockReturnValue(user);
-    const response = await request(app).post('/users/create');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(user);
-  });
-});
-
-describe('test /users/:id/update route', () => {
-  test('PUT /users/:id/update calls update and returns user', async () => {
-    prismaMock.user.update.mockReturnValue(user);
-    const response = await request(app)
-      .put('/users/4/update')
-      .send({ first_name: 'Update' });
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(user);
-    expect(prismaMock.user.update).toHaveBeenCalledWith({
+// /users/create
+const { userId: _u, coachId: _c, ...userProps } = user;
+testRoute({
+  reqData: {
+    ...user
+  },
+  mockData: user,
+  expectData: {
+    status: 200,
+    body: user,
+    calledWith: {
       data: {
-        first_name: 'Update'
-      },
-      where: { userId: '4' }
-    });
-  });
+        ...userProps,
+        coach: { connect: { userId: user.coachId } }
+      }
+    }
+  },
+  model: 'user',
+  route: 'create'
 });
 
-describe('test /users/:id/delete route', () => {
-  test('PUT /users/:id/delete calls delete and returns delete info', async () => {
-    prismaMock.user.delete.mockReturnValue(user);
-    const response = await request(app).delete('/users/4/delete');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(user);
-    expect(prismaMock.user.delete).toHaveBeenCalledWith({
-      where: { userId: '4' }
-    });
-  });
+// /users/update
+testRoute({
+  reqData: {
+    ...user
+  },
+  mockData: user,
+  expectData: {
+    status: 200,
+    body: user,
+    calledWith: {
+      data: {
+        ...user
+      },
+      where: {
+        userId: ':id'
+      }
+    }
+  },
+  model: 'user',
+  id: '1',
+  route: 'update'
+});
+
+// /users/delete
+testRoute({
+  reqData: {},
+  mockData: user,
+  expectData: {
+    status: 200,
+    body: user,
+    calledWith: {
+      where: {
+        userId: ':id'
+      }
+    }
+  },
+  model: 'user',
+  id: '1',
+  route: 'delete'
 });
