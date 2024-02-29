333c85fc0f0af16ef940ae050a44e9f084b356e3
Haruko Okada <hokada@uiowa.edu>
2024-02-28 22:23:13 -0600

    feat: made cell display start and end time

--- mobile/src/components/views/WindowCell.tsx
+++ mobile/src/components/views/WindowCell.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, Icon } from 'react-native-paper';
 
-interface CustomCellProps {
-  item: string;
+interface WindowCellProps {
+  startTime: string;
+  endTime: string;
 }
 
-const CustomCell: React.FC<CustomCellProps> = ({ item }) => {
+const WindowCell: React.FC<WindowCellProps> = ({ startTime, endTime }) => {
   return (
     <View style={styles.cell}>
-      <Text>{item}</Text>
+      <Text>{startTime}</Text>
+      <Text>{endTime}</Text>
+      <Icon
+        source="pencil"
+        size={20}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  cell: {}
+  cell: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+    marginHorizontal: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray'
+  }
 });
 
-export default CustomCell;
+export default WindowCell;
