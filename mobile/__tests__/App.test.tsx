7ef45878cd7a8b5a2ad665d152b01aac19466833
SergioMEV <sergiomarteloe@gmail.com>
2024-03-01 16:25:26 -0600

    test: mocks auth0Provider for app tests

--- mobile/__tests__/App.test.tsx
+++ mobile/__tests__/App.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import App from '../App';
+// import { Auth0Provider } from 'react-native-auth0';
+
+jest.mock('react-native-auth0', () => ({
+  Auth0Provider: ({ children }: { children: React.ReactNode }) => children
+}));
 
 describe('App', () => {
   it('renders without crashing', () => {
