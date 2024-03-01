76131eb0571f0bb8c51b470d3b4ae30d78011312
dlaurianti <dlaurianti@uiowa.edu>
2024-03-01 12:51:00 -0600

    test: sad & error paths for reminders

--- backend/test/api/reminders.test.ts
+++ backend/test/api/reminders.test.ts
@@ -1,3 +1,4 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { testRoute } from './common.js';
 
 const mockReminders = [
@@ -20,6 +21,9 @@ const reminder = {
   description: 'nap',
   timestamp: '2024-01-01T12:00:00Z'
 };
+
+// Happy Path Tests
+// ============================================================================
 
 // /reminders/all
 testRoute({
@@ -127,6 +131,259 @@ testRoute({
   expectData: {
     status: 200,
     body: reminder,
+    calledWith: {
+      where: {
+        reminderId: ':id'
+      }
+    }
+  },
+  model: 'reminder',
+  id: '1',
+  route: 'delete'
+});
+
+// Sad Path Tests
+// ============================================================================
+
+// /reminders/all
+testRoute({
+  description: 'returns no reminders if there are none',
+  reqData: {},
+  mockData: {},
+  expectData: {
+    status: 200,
+    body: {}
+  },
+  model: 'reminder',
+  route: 'all'
+});
+
+// /reminders/:id
+testRoute({
+  description: 'returns nothing if no reminder matches :id',
+  reqData: {},
+  mockData: {},
+  expectData: {
+    status: 200,
+    body: {},
+    calledWith: {
+      where: {
+        reminderId: ':id'
+      }
+    }
+  },
+  model: 'reminder',
+  id: '5'
+});
+
+// /reminders/search
+testRoute({
+  description: 'returns nothing if no reminders match searched role',
+  reqData: {
+    description: 'nap'
+  },
+  mockData: {},
+  expectData: {
+    status: 200,
+    body: {},
+    calledWith: {
+      where: {
+        description: 'nap'
+      }
+    }
+  },
+  model: 'reminder',
+  route: 'search'
+});
+
+// /reminders/create
+testRoute({
+  description: 'returns empty object when no data passed',
+  reqData: {},
+  mockData: {},
+  expectData: {
+    status: 200,
+    body: {},
+    calledWith: {
+      data: {
+        plan: { connect: { planId: undefined } },
+        description: undefined,
+        timestamp: null
+      }
+    }
+  },
+  model: 'reminder',
+  route: 'create'
+});
+
+// /reminders/update
+testRoute({
+  description: 'returns unupdated reminder if no data passed',
+  reqData: {},
+  mockData: reminder,
+  expectData: {
+    status: 200,
+    body: reminder,
+    calledWith: {
+      data: {},
+      where: {
+        reminderId: ':id'
+      }
+    }
+  },
+  model: 'reminder',
+  id: '1',
+  route: 'update'
+});
+
+// /reminders/delete
+testRoute({
+  description: 'returns nothing if no reminder matches :id',
+  reqData: {},
+  mockData: {},
+  expectData: {
+    status: 200,
+    body: {},
+    calledWith: {
+      where: {
+        reminderId: ':id'
+      }
+    }
+  },
+  model: 'reminder',
+  id: '5',
+  route: 'delete'
+});
+
+// Error Path Tests
+// ============================================================================
+
+const prismaError = new PrismaClientKnownRequestError(
+  'Error manually generated for testing',
+  {
+    code: 'P2010',
+    clientVersion: 'jest mock'
+  }
+);
+// /reminders/all
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    }
+  },
+  model: 'reminder',
+  route: 'all'
+});
+
+// /reminders/:id
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      where: {
+        reminderId: ':id'
+      }
+    }
+  },
+  model: 'reminder',
+  id: '1'
+});
+
+// /reminders/search
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      where: {}
+    }
+  },
+  model: 'reminder',
+  route: 'search'
+});
+
+// /reminders/create
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      data: {
+        plan: { connect: { planId: undefined } },
+        description: undefined,
+        timestamp: null
+      }
+    }
+  },
+  model: 'reminder',
+  route: 'create'
+});
+
+// /reminders/update
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      data: {},
+      where: {
+        reminderId: ':id'
+      }
+    }
+  },
+  model: 'reminder',
+  id: '1',
+  route: 'update'
+});
+
+// /reminders/delete
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
     calledWith: {
       where: {
         reminderId: ':id'
