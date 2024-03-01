3ddb531df99597036a4210998c523b212cd9a66a
Haruko Okada <hokada@uiowa.edu>
2024-02-29 20:04:26 -0600

    style: made button circular

--- mobile/src/components/buttons/TimerButton.tsx
+++ mobile/src/components/buttons/TimerButton.tsx
@@ -5,11 +5,15 @@
  */
 
 import React, { useState } from 'react';
-import { Button } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import type { StyleProp, ViewStyle } from 'react-native';
+import { TouchableRipple, Icon, Text } from 'react-native-paper';
+import { colors } from '../../../../common_styles/colors';
 
 interface TimerButtonProps {
   onStart: () => void;
   onStop: () => void;
+  style?: StyleProp<ViewStyle>;
 }
 
 /**
@@ -18,7 +22,11 @@ interface TimerButtonProps {
  * @param {function} props.onStart - The function to call when the timer starts.
  * @param {function} props.onStop - The function to call when the timer stops.
  */
-const TimerButton: React.FC<TimerButtonProps> = ({ onStart, onStop }) => {
+const TimerButton: React.FC<TimerButtonProps> = ({
+  onStart,
+  onStop,
+  style
+}) => {
   const [isTimerRunning, setIsTimerRunning] = useState(false);
 
   const handlePress = () => {
@@ -31,13 +39,57 @@ const TimerButton: React.FC<TimerButtonProps> = ({ onStart, onStop }) => {
   };
 
   return (
-    <Button
-      icon={isTimerRunning ? 'stop' : 'play'}
-      mode="contained"
-      onPress={handlePress}>
-      {isTimerRunning ? 'Stop' : 'Start'}
-    </Button>
+    <TouchableRipple
+      onPress={handlePress}
+      style={[styles.container, style]}>
+      <View style={styles.buttonContent}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>{isTimerRunning ? 'Stop' : 'Start'}</Text>
+        </View>
+        <View style={styles.iconContainer}>
+          <Icon
+            source={isTimerRunning ? 'stop' : 'play'}
+            size={30}
+          />
+        </View>
+      </View>
+    </TouchableRipple>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent'
+  },
+  buttonContent: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    borderRadius: 60,
+    backgroundColor: colors.crimsonRed
+  },
+  titleContainer: {
+    alignItems: 'center',
+    marginBottom: 50
+  },
+  title: {
+    fontSize: 16,
+    textAlign: 'center'
+  },
+  iconContainer: {
+    flex: 1,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default TimerButton;
