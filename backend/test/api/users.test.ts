136b1dfc32111f825aa8b633f6da6cd375f947aa
dlaurianti <dlaurianti@uiowa.edu>
2024-03-01 12:22:06 -0600

    test: error path tests for users

--- backend/test/api/users.test.ts
+++ backend/test/api/users.test.ts
@@ -1,3 +1,4 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { testRoute } from './common.js';
 import type { User } from '@prisma/client';
 
@@ -154,6 +155,7 @@ testRoute({
   id: '1',
   route: 'delete'
 });
+
 // Sad Path Tests
 // ============================================================================
 
@@ -273,5 +275,154 @@ testRoute({
   },
   model: 'user',
   id: '5',
+  route: 'delete'
+});
+
+// Error Path Tests
+// ============================================================================
+
+const prismaError = new PrismaClientKnownRequestError(
+  'Error manually generated for testing',
+  {
+    code: 'P2010',
+    clientVersion: 'jest mock'
+  }
+);
+// /users/all
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      include: {
+        Babies: true
+      }
+    }
+  },
+  model: 'user',
+  route: 'all'
+});
+
+// /users/:id
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      include: {
+        Babies: true
+      },
+      where: {
+        userId: ':id'
+      }
+    }
+  },
+  model: 'user',
+  id: '1'
+});
+
+// /users/search
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      where: {}
+    }
+  },
+  model: 'user',
+  route: 'search'
+});
+
+// /users/create
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      data: {
+        first_name: undefined,
+        last_name: undefined,
+        email: undefined,
+        role: undefined
+      }
+    }
+  },
+  model: 'user',
+  route: 'create'
+});
+
+// /users/update
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      data: {},
+      where: {
+        userId: ':id'
+      }
+    }
+  },
+  model: 'user',
+  id: '1',
+  route: 'update'
+});
+
+// /users/delete
+testRoute({
+  description: 'returns HTTP 500 if Prisma throws error',
+  reqData: {},
+  mockData: prismaError,
+  expectData: {
+    status: 500,
+    body: {
+      name: prismaError.name,
+      code: prismaError.code,
+      clientVersion: prismaError.clientVersion
+    },
+    calledWith: {
+      where: {
+        userId: ':id'
+      }
+    }
+  },
+  model: 'user',
+  id: '1',
   route: 'delete'
 });
