897093d229485e2cf5f02d2ad03832202719d84d
SergioMEV <sergiomarteloe@gmail.com>
2024-03-03 14:20:08 -0600

    feat: adds questions util function to mobile

--- mobile/src/utils/db.tsx
+++ mobile/src/utils/db.tsx
@@ -1,6 +1,9 @@
 import { getAuth0User, getUserCredentials } from './auth';
 
-// Fetches user data from db
+/**
+ * Fetches user data from the API.
+ * @returns {Promise<object | boolean>} The user data if successful, otherwise false.
+ */
 export const fetchUserData = async () => {
   try {
     const user = await getAuth0User();
@@ -21,6 +24,31 @@ export const fetchUserData = async () => {
         );
         const userData = await apiResponse.json();
         return userData;
+      }
+    }
+    return false;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+export const fetchQuestions = async () => {
+  try {
+    const userCredentials = await getUserCredentials();
+    if (userCredentials) {
+      const accessToken = userCredentials.accessToken;
+
+      if (accessToken) {
+        const apiResponse = await fetch(
+          process.env.EXPO_PUBLIC_API_URL + '/questions/all',
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+          }
+        );
+        const questions = await apiResponse.json();
+        return questions;
       }
     }
     return false;
