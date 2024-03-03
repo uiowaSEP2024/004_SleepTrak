4029704dda97da78c302f85636d35df1869ad9d1
Mingi Lee <mingilee98@gmail.com>
2024-03-03 17:36:58 -0600

    refactor: refactor Dialogs in Delete buttons for recommended schedule

--- web/src/components/RecommendedSchedule/ScheduleRowDeleteButton.tsx
+++ web/src/components/RecommendedSchedule/ScheduleRowDeleteButton.tsx
@@ -4,34 +4,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 
-export interface DeleteRowDialogueProps {
-  open: boolean;
-  onClose: () => void;
-}
-
-function DeleteRowDialogue(props: DeleteRowDialogueProps) {
-  const { open, onClose } = props;
-
-  return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-      sx={{ zIndex: '15000' }}>
-      <DialogTitle id="alert-dialog-title">{'Delete this row?'}</DialogTitle>
-      <DialogActions>
-        <Button onClick={onClose}>No</Button>
-        <Button
-          onClick={onClose}
-          autoFocus>
-          Yes
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-}
-
 export default function ScheduleDeleteRowButton() {
   const [open, setOpen] = React.useState(false);
 
@@ -52,10 +24,22 @@ export default function ScheduleDeleteRowButton() {
         onClick={handleClickOpen}>
         Delete
       </Button>
-      <DeleteRowDialogue
+      <Dialog
         open={open}
         onClose={handleClose}
-      />
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{ zIndex: '15000' }}>
+        <DialogTitle id="alert-dialog-title">{'Delete this row?'}</DialogTitle>
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+          <Button
+            onClick={handleClose}
+            autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
