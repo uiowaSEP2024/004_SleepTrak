892a5f2ddc06e85e62bdbdc24c062fc1e078274d
SergioMEV <sergiomarteloe@gmail.com>
2024-03-02 19:02:35 -0600

    fix: fixes issue with login screen redirecting

--- mobile/src/screens/OnboardingScreen.tsx
+++ mobile/src/screens/OnboardingScreen.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 const OnboardingScreen: React.FC = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>Onboarding Screen</Text>
+    </View>
+  );
 };
 
 export default OnboardingScreen;
