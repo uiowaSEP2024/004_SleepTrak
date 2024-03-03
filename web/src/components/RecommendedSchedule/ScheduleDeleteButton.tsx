f553e43dcf87f52810038c74b3bb74e336ee650e
Mingi Lee <mingilee98@gmail.com>
2024-03-03 17:36:58 -0600

    refactor: refactor Dialogs in Delete buttons for recommended schedule

--- web/src/components/RecommendedSchedule/ScheduleDeleteButton.tsx
+++ web/src/components/RecommendedSchedule/ScheduleDeleteButton.tsx
@@ -4,36 +4,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 
-export interface DeleteScheduleDialogueProps {
-  open: boolean;
-  onClose: () => void;
-}
-
-function DeleteScheduleDialogue(props: DeleteScheduleDialogueProps) {
-  const { open, onClose } = props;
-
-  return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-      sx={{ zIndex: '15000' }}>
-      <DialogTitle id="alert-dialog-title">
-        {'Delete this schedule?'}
-      </DialogTitle>
-      <DialogActions>
-        <Button onClick={onClose}>No</Button>
-        <Button
-          onClick={onClose}
-          autoFocus>
-          Yes
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-}
-
 export default function ScheduleDeleteButton() {
   const [open, setOpen] = React.useState(false);
 
@@ -55,10 +25,24 @@ export default function ScheduleDeleteButton() {
         onClick={handleClickOpen}>
         Delete Schedule
       </Button>
-      <DeleteScheduleDialogue
+      <Dialog
         open={open}
         onClose={handleClose}
-      />
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{ zIndex: '15000' }}>
+        <DialogTitle id="alert-dialog-title">
+          {'Delete this schedule?'}
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={handleClose}>No</Button>
+          <Button
+            onClick={handleClose}
+            autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
