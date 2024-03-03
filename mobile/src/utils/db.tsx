48fe31999838320bae5f402a4ba567bc64907f12
SergioMEV <sergiomarteloe@gmail.com>
2024-03-02 21:26:19 -0600

    feat: adds new userId management functions and updates db fetching function

--- mobile/src/utils/db.tsx
+++ mobile/src/utils/db.tsx
@@ -1,23 +1,26 @@
-import { getUserCredentials } from './auth';
+import { getAuth0User, getUserCredentials } from './auth';
 
 // Fetches user data from db
 export const fetchUserData = async () => {
   try {
+    const user = await getAuth0User();
     const userCredentials = await getUserCredentials();
-    if (userCredentials) {
-      const userId = userCredentials.idToken;
+    if (user && userCredentials) {
       const accessToken = userCredentials.accessToken;
 
       if (accessToken) {
         const apiResponse = await fetch(
-          process.env.EXPO_PUBLIC_API_URL + '/users/' + userId,
+          process.env.EXPO_PUBLIC_API_URL +
+            '/users/' +
+            (user as { sub: string }).sub,
           {
             headers: {
               Authorization: `Bearer ${accessToken}`
             }
           }
         );
-        return apiResponse;
+        const userData = await apiResponse.json();
+        return userData;
       }
     }
     return false;
