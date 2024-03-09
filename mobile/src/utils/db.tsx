b75b7f83a9e03234485eb6a03165732982bb7d98
SergioMEV <sergiomarteloe@gmail.com>
2024-03-09 17:28:35 -0600

    feat: adds db util functions for getting onboarding questions

--- mobile/src/utils/db.tsx
+++ mobile/src/utils/db.tsx
@@ -32,7 +32,11 @@ export const fetchUserData = async () => {
   }
 };
 
-export const fetchQuestions = async () => {
+/**
+ * Fetches onboarding questions from the API.
+ * @returns {Promise<Array<Object>>} - The array of onboarding questions.
+ */
+export const fetchOnboardingQuestions = async () => {
   try {
     const userCredentials = await getUserCredentials();
     if (userCredentials) {
@@ -49,6 +53,82 @@ export const fetchQuestions = async () => {
         );
         const questions = await apiResponse.json();
         return questions;
+      }
+    }
+    return false;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+/**
+ * Fetches onboarding questions for a specific screen number.
+ * @param screenNumber - The screen number to fetch questions for.
+ * @returns An array of onboarding questions for the specified screen number.
+ */
+export const fetchOnboardingQuestionsForScreen = async (
+  screenNumber: number
+) => {
+  const questions = await fetchOnboardingQuestions();
+  return questions.filter(
+    (question: { onboarding_screen: number }) =>
+      question.onboarding_screen === screenNumber
+  );
+};
+
+/**
+ * Creates onboarding answers in the database.
+ * @param answers - An array of objects containing the question ID, answer, and baby ID.
+ * @returns A Promise that resolves when all the onboarding answers are created.
+ */
+export const createOnboardingAnswers = async (
+  answers: Array<{ questionId: number; answer: string; babyId: string }>
+) => {
+  for (const answer of answers) {
+    await createOnboardingAnswer(
+      answer.questionId,
+      answer.answer,
+      answer.babyId
+    );
+  }
+};
+
+/**
+ * Creates an onboarding answer for a given question, answer, and baby ID.
+ * @param questionId - The ID of the question.
+ * @param answer - The answer to the question.
+ * @param babyId - The ID of the baby.
+ * @returns A Promise that resolves to the API response or false if there was an error.
+ */
+export const createOnboardingAnswer = async (
+  questionId: number,
+  answer: string,
+  babyId: string
+) => {
+  try {
+    const userCredentials = await getUserCredentials();
+    const user = await getAuth0User();
+    if (userCredentials) {
+      const accessToken = userCredentials.accessToken;
+
+      if (accessToken) {
+        const apiResponse = await fetch(
+          process.env.EXPO_PUBLIC_API_URL + '/answers/create',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${accessToken}`
+            },
+            body: JSON.stringify({
+              userId: (user as { sub: string }).sub,
+              babyId,
+              questionId,
+              answer
+            })
+          }
+        );
+        return apiResponse;
       }
     }
     return false;
