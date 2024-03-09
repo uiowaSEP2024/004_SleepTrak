7d058420914e95861204b79b84667c5ccd5f950a
SergioMEV <sergiomarteloe@gmail.com>
2024-03-09 17:31:48 -0600

    fix: fixes eslint issue with answers service

--- backend/src/services/answers.ts
+++ backend/src/services/answers.ts
@@ -2,13 +2,8 @@ import { prisma } from '../../prisma/client.js';
 import type { OnboardingAnswer } from '@prisma/client';
 import { ensureError } from '../utils/error.js';
 
-const create = async (answerData: {
-  answerId: string;
-  answer_text: string;
-  question: object;
-  user: object;
-  baby: object;
-}): Promise<OnboardingAnswer | Error> => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const create = async (answerData: any): Promise<OnboardingAnswer | Error> => {
   try {
     const result = await prisma.onboardingAnswer.create({ data: answerData });
 
