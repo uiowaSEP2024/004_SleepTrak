c8acb5b9b5cb50fdb645612dba5e85d2dbb323f2
Mingi Lee <mingilee98@gmail.com>
2024-03-13 17:38:50 -0500

    feat: add desiredBedTime to schedule create dialogue and add states to submit the form in ScheduleCreateButton.tsx

--- web/src/components/TimePickerField.tsx
+++ web/src/components/TimePickerField.tsx
@@ -3,24 +3,30 @@ import dayjs, { Dayjs } from 'dayjs';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { Box } from '@mui/joy';
 
-export interface TimePickerValueProps {
+interface TimePickerValueProps {
   label: string;
   defaultValue: Dayjs | null;
+  onChange: React.Dispatch<React.SetStateAction<dayjs.Dayjs | null>>;
 }
 
 export default function TimePickerField(props: TimePickerValueProps) {
-  const { label, defaultValue } = props;
+  const { label, defaultValue, onChange } = props;
 
   const [value, setValue] = React.useState<Dayjs | null>(defaultValue);
+
+  const handleOnChange = (
+    newValue: React.SetStateAction<dayjs.Dayjs | null>
+  ) => {
+    setValue(newValue);
+    onChange(newValue);
+  };
 
   return (
     <Box sx={{ my: '20px' }}>
       <TimePicker
         value={value}
         label={label}
-        onChange={(newValue: React.SetStateAction<dayjs.Dayjs | null>) =>
-          setValue(newValue)
-        }
+        onChange={handleOnChange}
       />
     </Box>
   );
