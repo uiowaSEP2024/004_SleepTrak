55ea62cdb0e4431646314be2f74be9fc24ea53b5
Mingi Lee <mingilee98@gmail.com>
2024-03-13 18:55:27 -0500

    feat: add recommended plan to seed.ts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -109,6 +109,141 @@ const questionData = [
   }
 ];
 
+// Recommended Plans
+//  We can figure out a better way to populate these later.
+const recommendedPlansData = [
+  {
+    ageInMonth: '1M',
+    numOfNaps: 5,
+    wakeWindow: 1
+  },
+  {
+    ageInMonth: '2M',
+    numOfNaps: 5,
+    wakeWindow: 1.25
+  },
+  {
+    ageInMonth: '3M',
+    numOfNaps: 5,
+    wakeWindow: 1.75
+  },
+  {
+    ageInMonth: '4M',
+    numOfNaps: 4,
+    wakeWindow: 2
+  },
+  {
+    ageInMonth: '5M',
+    numOfNaps: 4,
+    wakeWindow: 2.25
+  },
+  {
+    ageInMonth: '6M',
+    numOfNaps: 3,
+    wakeWindow: 2
+  },
+  {
+    ageInMonth: '7M',
+    numOfNaps: 3,
+    wakeWindow: 2.5
+  },
+  {
+    ageInMonth: '8M',
+    numOfNaps: 3,
+    wakeWindow: 3
+  },
+  {
+    ageInMonth: '9M',
+    numOfNaps: 2,
+    wakeWindow: 3
+  },
+  {
+    ageInMonth: '10M',
+    numOfNaps: 2,
+    wakeWindow: 3.25
+  },
+  {
+    ageInMonth: '11M',
+    numOfNaps: 2,
+    wakeWindow: 3.5
+  },
+  {
+    ageInMonth: '12M',
+    numOfNaps: 2,
+    wakeWindow: 3.75
+  },
+  {
+    ageInMonth: '13M',
+    numOfNaps: 2,
+    wakeWindow: 4
+  },
+  {
+    ageInMonth: '14M',
+    numOfNaps: 2,
+    wakeWindow: 4
+  },
+  {
+    ageInMonth: '15M',
+    numOfNaps: 2,
+    wakeWindow: 4
+  },
+  {
+    ageInMonth: '16M',
+    numOfNaps: 2,
+    wakeWindow: 4
+  },
+  {
+    ageInMonth: '17M',
+    numOfNaps: 2,
+    wakeWindow: 4
+  },
+  {
+    ageInMonth: '18M',
+    numOfNaps: 1,
+    wakeWindow: 5
+  },
+  {
+    ageInMonth: '19M',
+    numOfNaps: 1,
+    wakeWindow: 5.25
+  },
+  {
+    ageInMonth: '20M',
+    numOfNaps: 1,
+    wakeWindow: 5.5
+  },
+  {
+    ageInMonth: '21M',
+    numOfNaps: 1,
+    wakeWindow: 5.5
+  },
+  {
+    ageInMonth: '22M',
+    numOfNaps: 1,
+    wakeWindow: 5.5
+  },
+  {
+    ageInMonth: '23M',
+    numOfNaps: 1,
+    wakeWindow: 5.5
+  },
+  {
+    ageInMonth: '24M',
+    numOfNaps: 1,
+    wakeWindow: 5.75
+  },
+  {
+    ageInMonth: '25M',
+    numOfNaps: 1,
+    wakeWindow: 5.75
+  },
+  {
+    ageInMonth: '26M',
+    numOfNaps: 1,
+    wakeWindow: 6
+  }
+];
+
 function getRandomElement<T>(list: T[]): T {
   if (list.length === 0) {
     throw new Error('Error: The array is empty.');
@@ -176,6 +311,13 @@ async function main(): Promise<void> {
   for (const question of questionData) {
     await prisma.onboardingQuestion.create({
       data: question
+    });
+  }
+
+  // Create recommended plans
+  for (const recommendedPlanData of recommendedPlansData) {
+    await prisma.recommendedPlan.create({
+      data: recommendedPlanData
     });
   }
 
