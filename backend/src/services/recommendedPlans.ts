6627f46373c79e95663edcd4fe3462ea1c8946c1
Mingi Lee <mingilee98@gmail.com>
2024-03-13 19:57:08 -0500

    feat: make get request for recommended plan return based on babyId

--- backend/src/services/recommendedPlans.ts
+++ backend/src/services/recommendedPlans.ts
@@ -2,21 +2,48 @@ import { prisma } from '../../prisma/client.js';
 import type { RecommendedPlan } from '@prisma/client';
 import { ensureError } from '../utils/error.js';
 
-const get = async (
-  ageInMonth: string
-): Promise<RecommendedPlan | null | Error> => {
+function getAgeInMonth(dob: string): string {
+  const today = new Date();
+  const birthDate = new Date(dob);
+
+  let ageYears = today.getFullYear() - birthDate.getFullYear();
+  let ageMonths = today.getMonth() - birthDate.getMonth();
+
+  if (ageMonths < 0) {
+    ageYears--;
+    ageMonths += 12;
+  }
+
+  return ageYears * 12 + ageMonths + 'M';
+}
+
+// Get request gets the recommended schedule by babyId.
+const get = async (babyId: string): Promise<RecommendedPlan | null | Error> => {
   try {
-    const result = await prisma.recommendedPlan.findUnique({
+    // Find the baby by babyId
+    const baby = await prisma.baby.findUnique({
       where: {
-        ageInMonth
+        babyId: babyId
       }
     });
+
+    // Check if the baby exists
+    if (!baby) {
+      throw new Error(`Baby with ID ${babyId} not found`);
+    }
+
+    // Retrieve the recommended plan by the baby's age
+    const result = await prisma.recommendedPlan.findUnique({
+      where: {
+        ageInMonth: getAgeInMonth(baby.dob.toString())
+      }
+    });
+
     return result;
   } catch (err) {
     return ensureError(err);
   }
 };
-
 export const service = {
   get
 };
