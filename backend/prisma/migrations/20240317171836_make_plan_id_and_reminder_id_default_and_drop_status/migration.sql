f9b8ec64385e7b37a161a02954879befd360fc4c
Mingi Lee <mingilee98@gmail.com>
2024-03-17 12:20:04 -0500

    fix: make plan id and reminder id default and drop status in db

---
+++ backend/prisma/migrations/20240317171836_make_plan_id_and_reminder_id_default_and_drop_status/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Status` on the `Plan` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Plan" DROP COLUMN "Status";
