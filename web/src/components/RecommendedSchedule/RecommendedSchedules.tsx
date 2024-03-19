a602ff9a6a8d8cf7df5691baa5cdf43e867888f1
Mingi Lee <mingilee98@gmail.com>
2024-03-19 02:09:44 -0500

    fix: make the recommended schedules to fetch plans based on userId

--- web/src/components/RecommendedSchedule/RecommendedSchedules.tsx
+++ web/src/components/RecommendedSchedule/RecommendedSchedules.tsx
@@ -3,21 +3,31 @@ import ScheduleCreateButton from './ScheduleCreateButton';
 import RecommendedSchedule from './RecommendedSchedule';
 import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useParams } from 'react-router-dom';
 
 export default function RecommendedSchedules() {
   const [RecommendedSchedules, setRecommendedSchedules] = useState([]);
   const { getAccessTokenSilently } = useAuth0();
+  const { userId } = useParams(); // TODO: make plan related to babies not clients
 
   const fetchRecommendedSchedules = async () => {
     const token = await getAccessTokenSilently();
 
-    const response = await fetch('http://localhost:3000/plans/all', {
+    const searchParams = {
+      clientId: userId
+    };
+
+    const response = await fetch('http://localhost:3000/plans/search', {
+      method: 'POST',
       headers: {
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`
-      }
+      },
+      body: JSON.stringify(searchParams)
     });
 
     const data = await response.json();
+    console.log(data);
     setRecommendedSchedules(data);
   };
   useEffect(() => {
