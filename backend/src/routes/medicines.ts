df267ae0def4a1f3297e21953449f35388fdbb9b
SergioMEV <sergiomarteloe@gmail.com>
2024-03-18 21:37:20 -0500

    feat: adds medicine table to backend

---
+++ backend/src/routes/medicines.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { controller } from '../controllers/medicines.js';
+import { auth } from '../services/auth.js';
+import { asyncHandler } from './handler.js';
+
+const router = express.Router();
+
+// Protecting routes with auth0
+router.use(auth.requireAuth);
+
+// Routes
+router.post('/create', asyncHandler(controller.create));
+
+export default router;
---
+++ backend/src/services/medicines.ts
@@ -0,0 +1,22 @@
+import { prisma } from '../../prisma/client.js';
+import type { Medicine } from '@prisma/client';
+import { ensureError } from '../utils/error.js';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const create = async (answerData: any): Promise<Medicine | Error> => {
+  try {
+    const result = await prisma.medicine.upsert({
+      where: { medicineId: answerData.medicineId },
+      update: answerData,
+      create: answerData
+    });
+
+    return result;
+  } catch (err) {
+    return ensureError(err);
+  }
+};
+
+export const service = {
+  create
+};
