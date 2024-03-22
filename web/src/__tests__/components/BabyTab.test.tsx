34fde5bbb7f1f36ad51d0f45cb6e8c8615daf682
Mingi Lee <mingilee98@gmail.com>
2024-03-22 00:10:23 -0500

    test: change test for BabyTab according to changes in summary page

--- web/src/__tests__/components/BabyTab.test.tsx
+++ web/src/__tests__/components/BabyTab.test.tsx
@@ -3,6 +3,16 @@ import '../../util/setupDomTests';
 import { render, fireEvent } from '@testing-library/react';
 import BabyTab from '../../components/BabyTab';
 
+// Mock the RecommendedSchedules component
+jest.mock(
+  '../../components/RecommendedSchedule/RecommendedSchedules',
+  () => () => (
+    <div data-testid="mocked-recommended-schedules">
+      Mocked RecommendedSchedules
+    </div>
+  )
+);
+
 describe('BabyTab component', () => {
   it('renders correctly', () => {
     const { getByText } = render(<BabyTab />);
@@ -14,18 +24,10 @@ describe('BabyTab component', () => {
   it('switches tabs correctly', () => {
     const { getByText, getByTestId } = render(<BabyTab />);
     fireEvent.click(getByText('Summary'));
-    expect(getByTestId('summary-page')).toBeInTheDocument();
+    expect(getByTestId('mocked-recommended-schedules')).toBeInTheDocument();
     fireEvent.click(getByText('Log'));
     expect(getByTestId('log-page')).toBeInTheDocument();
     fireEvent.click(getByText('Docs'));
     expect(getByTestId('docs-page')).toBeInTheDocument();
-  });
-
-  it('renders correct content for active tab', () => {
-    const { getByText, queryByTestId } = render(<BabyTab />);
-    expect(getByText('Summary')).toBeInTheDocument();
-    expect(queryByTestId('summary-page')).toBeInTheDocument();
-    expect(queryByTestId('log-page')).toBeNull();
-    expect(queryByTestId('docs-page')).toBeNull();
   });
 });
