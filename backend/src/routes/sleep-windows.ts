8b6b50f9f753924660b9dc6f4626e312de3a0091
Haruko Okada <hokada@uiowa.edu>
2024-03-23 00:05:57 -0500

    feat: implemented routes for sleep-window

---
+++ backend/src/routes/sleep-windows.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { controller } from '../controllers/sleep-windows.js';
+import { auth } from '../services/auth.js';
+import { asyncHandler } from './handler.js';
+
+const router = express.Router();
+
+// Protecting routes with auth0
+router.use(auth.requireAuth);
+
+// Routes
+router.get('/:id', asyncHandler(controller.get));
+router.get(
+  '/sleep-windows/event/:eventId',
+  asyncHandler(controller.getByEventId)
+);
+router.post('/create', asyncHandler(controller.create));
+router.put('/:id/update', asyncHandler(controller.update));
+router.delete('/:id/delete', asyncHandler(controller.destroy));
+
+export default router;
