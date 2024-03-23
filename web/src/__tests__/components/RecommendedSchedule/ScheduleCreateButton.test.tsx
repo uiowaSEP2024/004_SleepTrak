5e0c9b680a9e58e9459111bca44953534603f3f9
Mingi Lee <mingilee98@gmail.com>
2024-03-23 15:49:02 -0500

    test: add tests for createSleepPlan and fetching data for ScheduleCreateButton

--- web/src/__tests__/components/RecommendedSchedule/ScheduleCreateButton.test.tsx
+++ web/src/__tests__/components/RecommendedSchedule/ScheduleCreateButton.test.tsx
@@ -1,23 +1,221 @@
 import '@testing-library/jest-dom';
 import '../../../util/setupDomTests';
-import { render, fireEvent, screen } from '@testing-library/react';
-import ScheduleCreateButton from '../../../components/RecommendedSchedule/ScheduleCreateButton';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import ScheduleCreateButton, {
+  WakeWindowInputField
+} from '../../../components/RecommendedSchedule/ScheduleCreateButton';
+import dayjs from 'dayjs';
+import { createSleepPlan } from '../../../util/utils';
 
 // Mock the TimePickerField component
 jest.mock('../../../components/TimePickerField', () => () => (
   <div data-testid="mocked-time-picker">Mocked TimePickerField</div>
 ));
 
+// Mock auth0
+jest.mock('@auth0/auth0-react', () => ({
+  ...jest.requireActual('@auth0/auth0-react'),
+  useAuth0: jest.fn().mockReturnValue({
+    isAuthenticated: true,
+    getAccessTokenSilently: jest.fn().mockResolvedValue('mocked-access-token')
+  })
+}));
+
+// Mock useParams
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn().mockReturnValue({ userId: '1', babyId: '1' })
+}));
+
+const MockRecommendedPlan = {
+  ageInMonth: '1M',
+  numOfNaps: 5,
+  wakeWindow: 3
+};
+
 describe('ScheduleCreateButton', () => {
   it('renders the create schedule dialog with correct elements', () => {
-    render(<ScheduleCreateButton />);
+    const { getByText } = render(<ScheduleCreateButton onSubmit={jest.fn()} />);
 
-    fireEvent.click(screen.getByText('Add Schedule'));
+    global.fetch = jest.fn().mockResolvedValue({
+      json: async () => MockRecommendedPlan
+    });
 
-    expect(screen.getByText('Create a new schedule')).toBeInTheDocument();
-    expect(screen.getByText('Number of Naps')).toBeInTheDocument();
-    expect(screen.getByText('Wake Windows')).toBeInTheDocument();
-    expect(screen.getByText('Cancel')).toBeInTheDocument();
-    expect(screen.getByText('Create')).toBeInTheDocument();
+    fireEvent.click(getByText('Add Schedule'));
+
+    expect(getByText('Create a new schedule')).toBeInTheDocument();
+    expect(getByText('Number of Naps')).toBeInTheDocument();
+    expect(getByText('Wake Windows (Hours Between Sleep)')).toBeInTheDocument();
+    expect(getByText('Cancel')).toBeInTheDocument();
+    expect(getByText('Create')).toBeInTheDocument();
+  });
+
+  it('fetches default recommended schedule when creating schedule', async () => {
+    const { getByText } = render(<ScheduleCreateButton onSubmit={jest.fn()} />);
+
+    global.fetch = jest.fn().mockResolvedValue({
+      json: async () => MockRecommendedPlan
+    });
+
+    fireEvent.click(getByText('Add Schedule'));
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledWith(
+        `http://localhost:3000/recommended_plans/1`,
+        {
+          headers: {
+            Authorization: `Bearer mocked-access-token`
+          }
+        }
+      );
+    });
+  });
+
+  it('submits the form correctly when click create button', async () => {
+    // Mock the createSleepPlan function for this test
+    const mockPlan = {
+      // planId: '1',
+      clientId: '1',
+      coachId: '1',
+      reminders: [
+        {
+          reminderId: '1',
+          planId: '1',
+          description: 'Reminder 1',
+          startTime: new Date('2024-02-27T08:00:00.000Z'),
+          endTime: new Date('2024-02-27T08:30:00.000Z')
+        }
+      ]
+    };
+
+    const utilsModule = await import('../../../util/utils');
+
+    const spyCreateSleepPlan = jest
+      .spyOn(utilsModule, 'createSleepPlan')
+      .mockReturnValue(mockPlan);
+
+    const { getByText } = render(<ScheduleCreateButton onSubmit={jest.fn()} />);
+
+    global.fetch = jest.fn().mockResolvedValue({
+      json: async () => MockRecommendedPlan
+    });
+
+    await fireEvent.click(getByText('Add Schedule'));
+
+    global.fetch = jest.fn(); // Mock the global fetch again for creating schedule
+
+    await fireEvent.click(getByText('Create'));
+
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledWith(
+        `http://localhost:3000/plans/create`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer mocked-access-token`
+          },
+          body: JSON.stringify(mockPlan)
+        }
+      );
+    });
+
+    spyCreateSleepPlan.mockRestore();
+  });
+});
+
+describe('WakeWindowInputField', () => {
+  it('renders with correct label and default value', () => {
+    const index = 0;
+    const label = 'Wake Window';
+    const wakeWindows = [2, 3, 4];
+    const onChange = jest.fn();
+
+    const { getByLabelText } = render(
+      <WakeWindowInputField
+        index={index}
+        label={label}
+        wakeWindows={wakeWindows}
+        onChange={onChange}
+      />
+    );
+
+    const input = getByLabelText(label) as HTMLInputElement;
+    expect(input).toBeInTheDocument();
+    expect(input.type).toBe('number');
+    expect(input.value).toBe('2');
+  });
+
+  it('calls onChange with updated wakeWindows when input value changes', () => {
+    const index = 1;
+    const label = 'Wake Window';
+    const wakeWindows = [2, 3, 4];
+    const onChange = jest.fn();
+
+    const { getByLabelText } = render(
+      <WakeWindowInputField
+        index={index}
+        label={label}
+        wakeWindows={wakeWindows}
+        onChange={onChange}
+      />
+    );
+
+    const input = getByLabelText(label) as HTMLInputElement;
+    fireEvent.change(input, { target: { value: '5' } });
+
+    expect(onChange).toHaveBeenCalledWith([2, 5, 4]);
+  });
+});
+
+describe('createSleepPlan', () => {
+  it('generates plan data with valid input parameters', () => {
+    const wakeUpTime = dayjs('2023-01-01T08:00');
+    const earliestGetReadyTime = dayjs('2023-01-01T19:30');
+    const desiredBedTime = dayjs('2023-01-01T20:00');
+    const wakeWindows = [3, 3, 3];
+    const numOfNaps = 2;
+    const userId = '123';
+    const user = { sub: '456' };
+
+    const planData = createSleepPlan(
+      wakeUpTime,
+      earliestGetReadyTime,
+      desiredBedTime,
+      wakeWindows,
+      numOfNaps,
+      userId,
+      user
+    );
+
+    const expectedReminders = [
+      {
+        description: 'Morning Rise',
+        startTime: '2023-01-01T14:00:00.000Z'
+      },
+      {
+        description: 'Nap 1',
+        startTime: '2023-01-01T17:00:00.000Z',
+        endTime: '2023-01-01T18:30:00.000Z'
+      },
+      {
+        description: 'Nap 2',
+        startTime: '2023-01-01T21:30:00.000Z',
+        endTime: '2023-01-01T23:00:00.000Z'
+      },
+      {
+        description: 'Get Ready for bed',
+        startTime: '2023-01-02T01:30:00.000Z'
+      },
+      {
+        description: 'Asleep ',
+        startTime: '2023-01-02T02:00:00.000Z'
+      }
+    ];
+
+    // Convert objects to JSON strings for comparison
+    const planDataString = JSON.stringify(planData.reminders);
+    const expectedRemindersString = JSON.stringify(expectedReminders);
+
+    expect(planDataString).toEqual(expectedRemindersString);
   });
 });
