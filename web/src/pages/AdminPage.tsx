586df773b5f36e1e3e78cd03662e35b15cf69043
dlaurianti <dlaurianti@uiowa.edu>
2024-03-26 23:08:50 -0500

    feat: added unassigned clients to admin page and made sections collapsible

--- web/src/pages/AdminPage.tsx
+++ web/src/pages/AdminPage.tsx
@@ -1,7 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import CoachCard from '../components/CoachCard';
-import Grid from '@mui/joy/Grid';
-import Item from '@mui/joy/Grid';
+import ClientCard from '../components/ClientCard';
+import { Grid, Box } from '@mui/joy';
+import Collapse from '@mui/material/Collapse';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useEffect, useState } from 'react';
 import { Button, styled } from '@mui/joy';
 import { Link as RouterLink } from 'react-router-dom';
@@ -25,6 +27,7 @@ interface User {
   last_name: string;
   role: string;
   babies: Baby[];
+  coachId?: string;
 }
 
 function AdminPage() {
@@ -49,40 +52,106 @@ function AdminPage() {
     fetchData();
   }, [getAccessTokenSilently]);
 
+  const [coachOpen, setCoachOpen] = useState(false);
+
+  const handleCoachExpandClick = () => {
+    setCoachOpen(!coachOpen);
+  };
+
+  const [clientOpen, setClientOpen] = useState(false);
+
+  const handleClientExpandClick = () => {
+    setClientOpen(!clientOpen);
+  };
+
   // TODO: filter so that the the page only shows current user's clients
   return (
     <>
-      <h2>Coaches</h2>
-      <Button
-        component={Link}
-        to="/admin"
-        type="button"
-        size="sm"
-        variant="soft"
-        color="danger"
-        sx={{ m: 2 }}>
-        Authorize New Coach
-      </Button>
-      <Grid
-        container
-        spacing={3}
-        sx={{ flexGrow: 1 }}>
-        {usersData
-          .filter((object) => object.role == 'coach')
-          .map((object, index) => (
-            <Grid
-              xs="auto"
-              key={index}>
-              <Item>
-                <CoachCard
-                  avatarSrc="testAvatar"
-                  title={object.first_name + ' ' + object.last_name}
-                  coachId={object.userId}
-                />
-              </Item>
-            </Grid>
-          ))}
-      </Grid>
+      <Box
+        onClick={handleCoachExpandClick}
+        sx={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+        <h2>Coaches</h2>
+        <ExpandMoreIcon
+          sx={{
+            transform: coachOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.3s'
+          }}
+        />
+      </Box>
+      <Collapse in={coachOpen}>
+        <Button
+          component={Link}
+          to="/admin"
+          type="button"
+          size="sm"
+          variant="soft"
+          color="danger"
+          sx={{ m: 2 }}>
+          Authorize New Coach
+        </Button>
+        <Grid
+          container
+          spacing={3}
+          sx={{ flexGrow: 1 }}>
+          {usersData
+            .filter((object) => object.role == 'coach')
+            .map((object, index) => (
+              <Grid
+                xs="auto"
+                key={index}>
+                <Box>
+                  <CoachCard
+                    avatarSrc="testAvatar"
+                    title={object.first_name + ' ' + object.last_name}
+                    coachId={object.userId}
+                  />
+                </Box>
+              </Grid>
+            ))}
+        </Grid>
+      </Collapse>
+
+      <Box
+        onClick={handleClientExpandClick}
+        sx={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+        <h2>Unassigned Clients</h2>
+        <ExpandMoreIcon
+          sx={{
+            transform: clientOpen ? 'rotate(180deg)' : 'rotate(0deg)',
+            transition: 'transform 0.3s'
+          }}
+        />
+      </Box>
+      <Collapse in={clientOpen}>
+        <Grid
+          container
+          spacing={3}
+          sx={{ flexGrow: 1 }}>
+          {usersData
+            .filter((object) => object.role == 'client')
+            .filter((object) => !object.coachId)
+            .map((object, index) => (
+              <Grid
+                xs="auto"
+                key={index}>
+                <Box>
+                  <ClientCard
+                    avatarSrc="testAvatar"
+                    title={object.first_name + ' ' + object.last_name}
+                    body={object.babies.map((baby) => baby.name).join(' ')}
+                    userId={object.userId || ''}
+                    babyId={
+                      object.babies.length > 0
+                        ? object.babies[0].babyId || ''
+                        : ''
+                    }
+                    adminOptions={true}
+                  />
+                </Box>
+              </Grid>
+            ))}
+        </Grid>
+      </Collapse>
     </>
   );
 }
