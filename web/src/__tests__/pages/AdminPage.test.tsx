8ce2cde0b134b83d8dd6afb40919982f4b7fb82b
dlaurianti <dlaurianti@uiowa.edu>
2024-03-29 14:56:48 -0500

    test: AdminPage tests now working with environment mocked

--- web/src/__tests__/pages/AdminPage.test.tsx
+++ web/src/__tests__/pages/AdminPage.test.tsx
@@ -41,6 +41,14 @@ global.fetch = jest.fn().mockResolvedValue({
   json: async () => mockUserData
 });
 
+// Mock environment variables
+jest.mock('../../util/environment.ts', () => ({
+  API_URL: 'localhost:3000',
+  DOMAIN: 'auth0domain',
+  CLIENT_ID: 'auth0clientid',
+  AUDIENCE: 'test-test'
+}));
+
 // Mock auth0
 jest.mock('@auth0/auth0-react', () => ({
   ...jest.requireActual('@auth0/auth0-react'),
--- web/src/util/environment.ts
+++ web/src/util/environment.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL!;
+const API_URL: string = import.meta.env.VITE_API_URL!;
 const DOMAIN: string = import.meta.env.VITE_AUTH0_DOMAIN!;
 const CLIENT_ID: string = import.meta.env.VITE_AUTH0_CLIENT_ID!;
 const AUDIENCE: string = import.meta.env.VITE_AUTH0_API_AUDIENCE;
