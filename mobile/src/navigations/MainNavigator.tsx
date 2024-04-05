60989609f5bb0fe8238706d1858ecfbc906865db
SergioMEV <sergiomarteloe@gmail.com>
2024-04-05 16:28:56 -0500

    feat: adds bottom nav bar and updates homestack to match navigation

--- mobile/src/navigations/MainNavigator.tsx
+++ mobile/src/navigations/MainNavigator.tsx
@@ -1,19 +1,76 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import SettingScreen from '../screens/SettingScreen';
 import HomeStack from './HomeStack';
+import { colors } from '../../assets/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import TestScreen from '../screens/TestScreen';
+import EventsScreen from '../screens/EventsScreen';
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.tan,
+        tabBarInactiveTintColor: 'white',
+        tabBarStyle: {
+          backgroundColor: colors.crimsonRed,
+          height: 60
+        }
+      }}>
       <Tab.Screen
-        name="Home_test"
+        name="Home"
         component={HomeStack}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="home-outline"
+              size={36}
+              color={color}
+            />
+          )
+        }}
       />
       <Tab.Screen
-        name="Setting"
-        component={SettingScreen}
+        name="Events"
+        component={EventsScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="calendar-outline"
+              size={36}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Statistics"
+        component={TestScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="chart-bar"
+              size={36}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={TestScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="chat-outline"
+              size={36}
+              color={color}
+            />
+          )
+        }}
       />
     </Tab.Navigator>
   );
