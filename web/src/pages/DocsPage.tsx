e4c544df221aa8de0003b54528a3358b612e8c75
Mingi Lee <mingilee98@gmail.com>
2024-04-08 02:05:30 -0500

    feat: create FilesList to display current baby's files in the frontend

--- web/src/pages/DocsPage.tsx
+++ web/src/pages/DocsPage.tsx
@@ -1,7 +1,51 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import FileUploadButton from '../components/FileUploadButton';
+import FilesList from '../components/FilesList';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+export interface File {
+  fildId: string;
+  filename: string;
+  url: string;
+}
 
 function DocsPage() {
-  return <FileUploadButton />;
+  const [files, setFiles] = useState([]);
+  const { getAccessTokenSilently } = useAuth0();
+  const { babyId } = useParams(); // Access the userId from the route parameters
+
+  useEffect(() => {
+    const fetchFilesData = async () => {
+      const token = await getAccessTokenSilently();
+
+      const searchParams = {
+        babyId
+      };
+
+      const response = await fetch('http://localhost:3000/files/search', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(searchParams)
+      });
+
+      const data = await response.json();
+      setFiles(data);
+      console.log(data);
+    };
+
+    fetchFilesData();
+  }, [getAccessTokenSilently, babyId]);
+
+  return (
+    <>
+      <FileUploadButton />
+      <FilesList files={files} />
+    </>
+  );
 }
 
 export default DocsPage;
