0b53fa2d51d7102617f59969d3d1fa768bf48911
SergioMEV <sergiomarteloe@gmail.com>
2024-04-07 20:40:42 -0500

    feat: adds barebones setting screen

--- mobile/src/screens/SettingScreen.tsx
+++ mobile/src/screens/SettingScreen.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import { colors } from '../../assets/colors';
+import { LogoutButton } from '../components/buttons/AuthButtons';
 
 function Setting() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Setting!</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Settings</Text>
+      <LogoutButton style={styles.logoutButton} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-start',
+    backgroundColor: 'white',
+    paddingHorizontal: '7%',
+    paddingVertical: '2%'
+  },
+  title: {
+    color: colors.textGray,
+    alignSelf: 'flex-start',
+    fontSize: 32,
+    letterSpacing: 2
+  },
+  logoutButton: {
+    position: 'absolute',
+    alignSelf: 'center',
+    alignContent: 'center',
+    justifyContent: 'center',
+    bottom: '6%',
+    backgroundColor: colors.crimsonRed,
+    width: '70%',
+    height: '7%',
+    fontSize: 20,
+    color: 'white',
+    borderRadius: 32
   }
 });
 
