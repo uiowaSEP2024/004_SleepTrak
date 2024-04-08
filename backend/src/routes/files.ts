eac3d2ea95d033f594bc0834e3be815d7e8cf446
Mingi Lee <mingilee98@gmail.com>
2024-04-08 01:23:07 -0500

    feat: create backend api for creating a file and uploading it to S3 bucket

---
+++ backend/src/routes/files.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { controller } from '../controllers/files.js';
+import { auth } from '../services/auth.js';
+import { asyncHandler } from './handler.js';
+import multer from 'multer';
+
+const router = express.Router();
+
+// Set up Multer for file upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// Protecting routes with auth0
+router.use(auth.requireAuth);
+
+// Routes
+router.post('/create', upload.single('file'), asyncHandler(controller.create));
+
+export default router;
