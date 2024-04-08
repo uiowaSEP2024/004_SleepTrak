2b37106aed705ef9bfb28663846068fd02dd9825
dlaurianti <dlaurianti@uiowa.edu>
2024-04-07 22:05:05 -0500

    feat: conversation adder component

---
+++ web/src/components/ConversationAdder.tsx
@@ -0,0 +1,90 @@
+import { FormControl, Box, Button } from '@mui/joy';
+import { useEffect, useState } from 'react';
+import {
+  Conversation,
+  Client as ConversationsClient
+} from '@twilio/conversations';
+import UserSearch from '../components/UserSearch';
+
+interface User {
+  userId: string;
+  first_name: string;
+  last_name: string;
+  role: string;
+  email: string;
+}
+
+interface ConversationAdderProps {
+  conversationsClient: ConversationsClient;
+}
+
+const ConversationAdder: React.FC<ConversationAdderProps> = ({
+  conversationsClient
+}) => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  // const handleSubmit = () => {
+
+  const handleUserSearchSelection = (user: User | null) => {
+    setSelectedUser(user);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!selectedUser) {
+      console.log('No user selected');
+      return;
+    }
+    if (await hasConversationWith(selectedUser)) {
+      console.log('Already have conversation with user');
+      return;
+    }
+    const newConversation: Conversation =
+      await conversationsClient.createConversation();
+    await newConversation.join();
+    try {
+      await newConversation.add(selectedUser.userId);
+    } catch {
+      console.log(`Failed to add ${selectedUser.userId}`);
+    }
+    console.log(newConversation);
+  };
+
+  async function hasConversationWith(user: User) {
+    const paginator = await conversationsClient.getSubscribedConversations();
+    const conversations: Conversation[] = [];
+    while (paginator.hasNextPage) {
+      conversations.concat(paginator.items);
+    }
+    const participants = (
+      await Promise.all(
+        conversations.map((conversation) => conversation.getParticipants())
+      )
+    ).flat();
+    console.log(participants);
+    console.log(user);
+    const hasConversation = participants.some(
+      (participant) => participant.identity == user.userId
+    );
+    return hasConversation;
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Box
+          display="flex"
+          alignItems="center">
+          <Button
+            type="submit"
+            variant="soft"
+            sx={{ margin: 2 }}>
+            Create Conversation with
+          </Button>
+          <UserSearch onChange={handleUserSearchSelection} />
+        </Box>
+      </form>
+    </>
+  );
+};
+
+export default ConversationAdder;
