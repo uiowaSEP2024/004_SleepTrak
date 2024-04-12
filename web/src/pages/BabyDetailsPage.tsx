ba5edae095542ec7068319c2d122091e340e1333
dlaurianti <dlaurianti@uiowa.edu>
2024-04-12 17:55:24 -0500

    refactor: getName util function

--- web/src/pages/BabyDetailsPage.tsx
+++ web/src/pages/BabyDetailsPage.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import API_URL from '../util/apiURL';
 import { UserWithBabies } from '../types/schemaExtensions';
+import { getName } from '../util/utils';
 
 export default function BabyDetailsPage() {
   const [clientData, setClientData] = useState<UserWithBabies | null>(null);
@@ -32,7 +33,7 @@ export default function BabyDetailsPage() {
   if (clientData && clientData.first_name && clientData.babies) {
     return (
       <Box>
-        <h2>{clientData.first_name + ' ' + clientData.last_name}</h2>
+        <h2>{getName(clientData)}</h2>
         <Box display="flex">
           <BabyDropdown babies={clientData.babies} />
         </Box>
