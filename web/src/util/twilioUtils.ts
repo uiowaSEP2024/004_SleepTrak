6b6d10a5b1571b3f1c507f2fa629e5a8002d427b
dlaurianti <dlaurianti@uiowa.edu>
2024-04-12 16:09:21 -0500

    feat: makeConversation & hasParticipant util functions

--- web/src/util/twilioUtils.ts
+++ web/src/util/twilioUtils.ts
@@ -1,4 +1,8 @@
-import { Paginator } from '@twilio/conversations';
+import {
+  Conversation,
+  Client as ConversationsClient,
+  Paginator
+} from '@twilio/conversations';
 
 export const getPaginatorItems = <T>(paginator: Paginator<T>) => {
   const items: T[] = [];
@@ -7,4 +11,27 @@ export const getPaginatorItems = <T>(paginator: Paginator<T>) => {
     items.push(...paginator.items);
   }
   return items;
+};
+
+export const makeConversation = async (
+  conversationsClient: ConversationsClient,
+  uid: string
+) => {
+  const newConversation: Conversation =
+    await conversationsClient.createConversation();
+  await newConversation.join();
+  try {
+    await newConversation.add(uid);
+  } catch {
+    newConversation.delete();
+  }
+};
+
+export const hasParticipant = async (
+  conversation: Conversation,
+  uid: string
+) => {
+  return (await conversation.getParticipants()).some(
+    (participant) => participant.identity === uid
+  );
 };
