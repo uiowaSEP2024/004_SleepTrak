b2055e53e0dca5101afc29ad5cb6696a40d16f29
Haruko Okada <hokada@uiowa.edu>
2024-04-13 15:30:57 -0500

    feat: filter event by type and pass to each statistic page

--- mobile/src/screens/StatisticScreen.tsx
+++ mobile/src/screens/StatisticScreen.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import type { RouteProp } from '@react-navigation/native';
+import type { StatisticStackParamList } from '../navigations/StatisticStack';
 
-const StatisticScreen: React.FC = () => {
+type StatisticScreenRouteProp = RouteProp<
+  StatisticStackParamList,
+  'StatisticScreen'
+>;
+
+interface StatisticScreenProps {
+  route: StatisticScreenRouteProp;
+}
+
+const StatisticScreen: React.FC<StatisticScreenProps> = ({ route }) => {
+  const { events } = route.params;
   return (
     <View>
       <Text>Statistic Screen</Text>
+      {events.map((event, index) => (
+        <Text key={index}>{event.eventId}</Text>
+      ))}
     </View>
   );
 };
--- mobile/src/screens/StatisticsScreen.tsx
+++ mobile/src/screens/StatisticsScreen.tsx
@@ -1,10 +1,66 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { fetchEvents } from '../utils/db';
+import type { StackNavigationProp } from '@react-navigation/stack';
+import type { StatisticStackParamList } from '../navigations/StatisticStack';
+import type { RemoteEvent } from '../utils/interfaces';
 
 const StatisticsScreen: React.FC = () => {
+  const navigation =
+    useNavigation<
+      StackNavigationProp<StatisticStackParamList, 'StatisticsScreen'>
+    >();
+  const [events, setEvents] = useState<RemoteEvent[] | null>(null);
+
+  useEffect(() => {
+    const fetchAndSetEvents = async () => {
+      try {
+        const fetchedEvents = await fetchEvents();
+        setEvents(fetchedEvents);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    void fetchAndSetEvents();
+  }, []);
+
+  // filter events into their types
+  const nightSleepEvents =
+    events?.filter((event) => event.type === 'night_sleep') ?? [];
+  const napEvents = events?.filter((event) => event.type === 'nap') ?? [];
+  const feedEvents = events?.filter((event) => event.type === 'feed') ?? [];
+
   return (
     <View>
-      <Text>Statistics Screen</Text>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('StatisticScreen', {
+            eventType: 'night_sleep',
+            events: nightSleepEvents
+          });
+        }}>
+        <Text>Night Sleep</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('StatisticScreen', {
+            eventType: 'nap',
+            events: napEvents
+          });
+        }}>
+        <Text>Nap</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('StatisticScreen', {
+            eventType: 'feed',
+            events: feedEvents
+          });
+        }}>
+        <Text>Feed</Text>
+      </TouchableOpacity>
     </View>
   );
 };
