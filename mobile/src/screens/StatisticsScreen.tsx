ff85ee740f4d734075502befef2051e735915c29
Haruko Okada <hokada@uiowa.edu>
2024-04-14 13:19:26 -0500

    feat: attempt line chart

--- mobile/src/screens/StatisticsScreen.tsx
+++ mobile/src/screens/StatisticsScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { fetchEvents } from '../utils/db';
 import type { StackNavigationProp } from '@react-navigation/stack';
 import type { StatisticStackParamList } from '../navigations/StatisticStack';
 import type { RemoteEvent } from '../utils/interfaces';
+import { colors } from '../../assets/colors';
 
 const StatisticsScreen: React.FC = () => {
   const navigation =
@@ -33,17 +34,19 @@ const StatisticsScreen: React.FC = () => {
   const feedEvents = events?.filter((event) => event.type === 'feed') ?? [];
 
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity
-        onPress={() => {
-          navigation.navigate('StatisticScreen', {
-            eventType: 'night_sleep',
-            events: nightSleepEvents
-          });
-        }}>
-        <Text>Night Sleep</Text>
-      </TouchableOpacity>
+        style={styles.navigationButton}
+          onPress={() => {
+            navigation.navigate('StatisticScreen', {
+              eventType: 'night_sleep',
+              events: nightSleepEvents
+            });
+          }}>
+          <Text>Night Sleep</Text>
+        </TouchableOpacity>
       <TouchableOpacity
+        style={styles.navigationButton}
         onPress={() => {
           navigation.navigate('StatisticScreen', {
             eventType: 'nap',
@@ -53,6 +56,7 @@ const StatisticsScreen: React.FC = () => {
         <Text>Nap</Text>
       </TouchableOpacity>
       <TouchableOpacity
+        style={styles.navigationButton}
         onPress={() => {
           navigation.navigate('StatisticScreen', {
             eventType: 'feed',
@@ -64,5 +68,29 @@ const StatisticsScreen: React.FC = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 50,
+    marginBottom: 50
+  },
+  navigationButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.softLavender,
+    padding: 10,
+    margin: 10,
+    borderRadius: 48,
+    width: '80%',
+    shadowOffset: { width: 0, height: 2 },
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+    shadowRadius: 4
+  }
+});
 
 export default StatisticsScreen;
