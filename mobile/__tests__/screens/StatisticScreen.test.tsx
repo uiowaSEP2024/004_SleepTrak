2a4c64660906be347be1528fd18c5784f0ce0a21
Haruko Okada <hokada@uiowa.edu>
2024-04-21 16:52:21 -0500

    test: wrote more tests and refactored some

--- mobile/__tests__/screens/StatisticScreen.test.tsx
+++ mobile/__tests__/screens/StatisticScreen.test.tsx
@@ -1,225 +1,308 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import StatisticScreen from '../../src/screens/StatisticScreen';
-import type { StatisticStackParamList } from '../../src/navigations/StatisticStack';
-import type { RouteProp } from '@react-navigation/native';
-// import type { RemoteEvent, RemoteSleepWindow } from '../../src/utils/interfaces';
+import type { RemoteEvent } from '../../src/utils/interfaces';
 
-// function generateEvent(eventId: string, type: string, startTime: Date, endTime?: Date | null, sleepWindows?: RemoteSleepWindow[] | null): RemoteEvent {
-//   return {
-//     eventId: eventId,
-//     ownerId: '100',
-//     startTime: startTime,
-//     endTime: endTime,
-//     type: type,
-//     sleepWindows: sleepWindows
-//   }
-// }
-
-const feedRoute: RouteProp<StatisticStackParamList, 'StatisticScreen'> = {
-  key: 'StatisticScreen',
-  name: 'StatisticScreen',
-  params: {
-    eventType: 'feed',
-    events: [
-      {
-        eventId: '1',
-        ownerId: '2',
-        startTime: new Date(),
-        type: 'feed'
-      },
-      {
-        eventId: '2',
-        ownerId: '2',
-        startTime: new Date(),
-        type: 'feed'
-      }
-    ]
+const feedEvents: RemoteEvent[] = [
+  {
+    eventId: '100',
+    ownerId: '2',
+    startTime: new Date('2024-04-15T10:00:00Z'),
+    type: 'feed'
+  },
+  {
+    eventId: '200',
+    ownerId: '2',
+    startTime: new Date('2024-04-15T13:00:00Z'),
+    type: 'feed'
+  },
+  {
+    eventId: '300',
+    ownerId: '2',
+    startTime: new Date('2024-04-15T16:00:00Z'),
+    type: 'feed'
+  },
+  {
+    eventId: '400',
+    ownerId: '2',
+    startTime: new Date('2024-04-16T10:00:00Z'),
+    type: 'feed'
+  },
+  {
+    eventId: '500',
+    ownerId: '2',
+    startTime: new Date('2024-04-16T02:00:00Z'),
+    type: 'feed'
   }
-};
-const napRoute: RouteProp<StatisticStackParamList, 'StatisticScreen'> = {
-  key: 'StatisticScreen',
-  name: 'StatisticScreen',
-  params: {
-    eventType: 'nap',
-    events: [
+];
+
+const napEvents: RemoteEvent[] = [
+  {
+    eventId: '300',
+    ownerId: '2',
+    startTime: new Date('2024-04-15T13:00:00Z'),
+    endTime: new Date('2024-04-15T16:00:00Z'),
+    type: 'nap',
+    sleepWindows: [
       {
-        // NAP FROM 13:00 TO 16:00 (3 HOURS) ON 4/15
+        windowId: '1',
         eventId: '300',
-        ownerId: '2',
-        startTime: new Date('2024-04-15T13:00:00'),
-        endTime: new Date('2024-04-15T16:00:00'),
-        type: 'nap',
-        sleepWindows: [
-          {
-            windowId: '1',
-            eventId: '300',
-            startTime: new Date('2024-04-15T13:00:00'),
-            stopTime: new Date('2024-04-15T14:00:00'),
-            isSleep: true,
-            note: ''
-          },
-          {
-            windowId: '2',
-            eventId: '300',
-            startTime: new Date('2024-04-15T14:00:00'),
-            stopTime: new Date('2024-04-15T15:00:00'),
-            isSleep: false,
-            note: ''
-          },
-          {
-            windowId: '3',
-            eventId: '300',
-            startTime: new Date('2024-04-15T15:00:00'),
-            stopTime: new Date('2024-04-15T16:00:00'),
-            isSleep: true,
-            note: ''
-          }
-        ]
+        startTime: new Date('2024-04-15T13:00:00Z'),
+        stopTime: new Date('2024-04-15T14:00:00Z'),
+        isSleep: true,
+        note: ''
       },
       {
-        // NAP FROM 13:00 TO 16:00 (3 HOURS) ON 4/16
+        windowId: '2',
+        eventId: '300',
+        startTime: new Date('2024-04-15T14:00:00Z'),
+        stopTime: new Date('2024-04-15T15:00:00Z'),
+        isSleep: false,
+        note: ''
+      },
+      {
+        windowId: '3',
+        eventId: '300',
+        startTime: new Date('2024-04-15T15:00:00Z'),
+        stopTime: new Date('2024-04-15T16:00:00Z'),
+        isSleep: true,
+        note: ''
+      }
+    ]
+  },
+  {
+    eventId: '400',
+    ownerId: '2',
+    startTime: new Date('2024-04-16T13:00:00Z'),
+    endTime: new Date('2024-04-16T16:00:00Z'),
+    type: 'nap',
+    sleepWindows: [
+      {
+        windowId: '4',
         eventId: '400',
-        ownerId: '2',
-        startTime: new Date('2024-04-16T13:00:00'),
-        endTime: new Date('2024-04-16T16:00:00'),
-        type: 'nap',
-        sleepWindows: [
-          {
-            windowId: '4',
-            eventId: '400',
-            startTime: new Date('2024-04-16T13:00:00'),
-            stopTime: new Date('2024-04-16T14:00:00'),
-            isSleep: true,
-            note: ''
-          },
-          {
-            windowId: '5',
-            eventId: '400',
-            startTime: new Date('2024-04-15T14:00:00'),
-            stopTime: new Date('2024-04-15T15:00:00'),
-            isSleep: false,
-            note: ''
-          },
-          {
-            windowId: '6',
-            eventId: '400',
-            startTime: new Date('2024-04-15T15:00:00'),
-            stopTime: new Date('2024-04-15T16:00:00'),
-            isSleep: true,
-            note: ''
-          }
-        ]
+        startTime: new Date('2024-04-16T13:00:00Z'),
+        stopTime: new Date('2024-04-16T14:00:00Z'),
+        isSleep: true,
+        note: ''
       },
       {
-        // NAP FROM 10:00 TO 12:00 (2 HOURS) ON 4/16
+        windowId: '5',
+        eventId: '400',
+        startTime: new Date('2024-04-16T14:00:00Z'),
+        stopTime: new Date('2024-04-16T15:00:00Z'),
+        isSleep: false,
+        note: ''
+      },
+      {
+        windowId: '6',
+        eventId: '400',
+        startTime: new Date('2024-04-16T15:00:00Z'),
+        stopTime: new Date('2024-04-16T16:00:00Z'),
+        isSleep: true,
+        note: ''
+      }
+    ]
+  },
+  {
+    eventId: '500',
+    ownerId: '2',
+    startTime: new Date('2024-04-16T10:00:00Z'),
+    endTime: new Date('2024-04-16T12:00:00Z'),
+    type: 'nap',
+    sleepWindows: [
+      {
+        windowId: '4',
         eventId: '500',
-        ownerId: '2',
-        startTime: new Date('2024-04-16T10:00:00'),
-        endTime: new Date('2024-04-16T12:00:00'),
-        type: 'nap',
-        sleepWindows: [
-          {
-            windowId: '4',
-            eventId: '500',
-            startTime: new Date('2024-04-16T10:00:00'),
-            stopTime: new Date('2024-04-16T12:00:00'),
-            isSleep: true,
-            note: ''
-          }
-        ]
-      },
+        startTime: new Date('2024-04-16T10:00:00Z'),
+        stopTime: new Date('2024-04-16T12:00:00Z'),
+        isSleep: true,
+        note: ''
+      }
+    ]
+  },
+  {
+    eventId: '600',
+    ownerId: '2',
+    startTime: new Date('2024-04-17T15:00:00Z'),
+    endTime: new Date('2024-04-17T16:00:00Z'),
+    type: 'nap',
+    sleepWindows: [
       {
-        // NAP FROM 15:00 TO 16:00 (1 HOUR) ON 4/17
+        windowId: '4',
         eventId: '600',
-        ownerId: '2',
-        startTime: new Date('2024-04-16T17:00:00'),
-        endTime: new Date('2024-04-16T17:00:00'),
-        type: 'nap',
-        sleepWindows: [
-          {
-            windowId: '4',
-            eventId: '600',
-            startTime: new Date('2024-04-17T15:00:00'),
-            stopTime: new Date('2024-04-17T16:00:00'),
-            isSleep: true,
-            note: ''
-          }
-        ]
+        startTime: new Date('2024-04-17T15:00:00Z'),
+        stopTime: new Date('2024-04-17T16:00:00Z'),
+        isSleep: true,
+        note: ''
       }
     ]
   }
-};
-const nightSleepRoute: RouteProp<StatisticStackParamList, 'StatisticScreen'> = {
-  key: 'StatisticScreen',
-  name: 'StatisticScreen',
-  params: {
-    eventType: 'night_sleep',
-    events: [
+];
+
+const nightSleepEvents: RemoteEvent[] = [
+  {
+    eventId: '300',
+    ownerId: '1',
+    startTime: new Date('2024-04-14T21:00:00Z'),
+    endTime: new Date('2024-04-15T07:00:00Z'),
+    type: 'night_sleep',
+    sleepWindows: [
       {
-        eventId: '1',
-        ownerId: '2',
-        startTime: new Date(),
-        type: 'night_sleep'
+        windowId: '1',
+        eventId: '300',
+        startTime: new Date('2024-04-14T21:00:00Z'),
+        stopTime: new Date('2024-04-14T23:00:00Z'),
+        isSleep: true,
+        note: ''
       },
       {
-        eventId: '2',
-        ownerId: '2',
-        startTime: new Date(),
-        type: 'night_sleep'
+        windowId: '2',
+        eventId: '300',
+        startTime: new Date('2024-04-14T23:00:00Z'),
+        stopTime: new Date('2024-04-15T01:00:00Z'),
+        isSleep: false,
+        note: ''
+      },
+      {
+        windowId: '3',
+        eventId: '300',
+        startTime: new Date('2024-04-15T01:00:00Z'),
+        stopTime: new Date('2024-04-15T03:00:00Z'),
+        isSleep: true,
+        note: ''
+      },
+      {
+        windowId: '4',
+        eventId: '300',
+        startTime: new Date('2024-04-15T03:00:00Z'),
+        stopTime: new Date('2024-04-15T04:00:00Z'),
+        isSleep: false,
+        note: ''
+      },
+      {
+        windowId: '5',
+        eventId: '300',
+        startTime: new Date('2024-04-15T04:00:00Z'),
+        stopTime: new Date('2024-04-15T07:00:00Z'),
+        isSleep: true,
+        note: ''
+      }
+    ]
+  },
+  {
+    eventId: '400',
+    ownerId: '1',
+    startTime: new Date('2024-04-15T23:00:00Z'),
+    endTime: new Date('2024-04-16T06:00:00Z'),
+    type: 'night_sleep',
+    sleepWindows: [
+      {
+        windowId: '1',
+        eventId: '400',
+        startTime: new Date('2024-04-15T23:00:00Z'),
+        stopTime: new Date('2024-04-16T6:00:00Z'),
+        isSleep: true,
+        note: ''
       }
     ]
   }
-};
+];
 
-const emptyRoute: RouteProp<StatisticStackParamList, 'StatisticScreen'> = {
-  key: 'StatisticScreen',
-  name: 'StatisticScreen',
-  params: {
-    eventType: 'feed',
-    events: []
-  }
-};
+const emptyEvent: RemoteEvent[] = [];
 
 describe('<StatisticScreen />', () => {
   describe('renders', () => {
     it('correctly with empty events', () => {
-      const { getByText } = render(<StatisticScreen route={emptyRoute} />);
+      const { getByText } = render(<StatisticScreen events={emptyEvent} />);
       expect(getByText('There are no statistics to display yet')).toBeTruthy();
     });
     it('nap statistic screen correctly', () => {
-      const { getByText } = render(<StatisticScreen route={napRoute} />);
-      expect(getByText('Nap Statistics')).toBeTruthy();
+      const { getByText } = render(<StatisticScreen events={napEvents} />);
+      expect(getByText('Average Summary')).toBeTruthy();
     });
     it('night sleep statistic screen correctly', () => {
-      const { getByText } = render(<StatisticScreen route={nightSleepRoute} />);
-      expect(getByText('Night Sleep Statistics')).toBeTruthy();
+      const { getByText } = render(
+        <StatisticScreen events={nightSleepEvents} />
+      );
+      expect(getByText('Average Summary')).toBeTruthy();
     });
     it('feed statistic screen correctly', () => {
-      const { getByText } = render(<StatisticScreen route={feedRoute} />);
-      expect(getByText('Feed Statistics')).toBeTruthy();
+      const { getByText } = render(<StatisticScreen events={feedEvents} />);
+      expect(getByText('Average Summary')).toBeTruthy();
     });
   });
 
   describe('summary card', () => {
     it('renders correctly', () => {
-      const { getByText } = render(<StatisticScreen route={napRoute} />);
+      const { getByText } = render(<StatisticScreen events={napEvents} />);
       expect(getByText('Average Summary')).toBeTruthy();
     });
     it('calculates average nap time correctly', () => {
-      const { getByText } = render(<StatisticScreen route={napRoute} />);
+      const { getByText } = render(<StatisticScreen events={napEvents} />);
       expect(getByText('Nap Time')).toBeTruthy();
       expect(getByText('2 h 15 m')).toBeTruthy();
     });
     it('calculates average nap sleep time correctly', () => {
-      const { getByText } = render(<StatisticScreen route={napRoute} />);
+      const { getByText } = render(<StatisticScreen events={napEvents} />);
       expect(getByText('Nap Time (Sleep)')).toBeTruthy();
       expect(getByText('1 h 45 m')).toBeTruthy();
     });
     it('calculates average number of naps correctly', () => {
-      const { getByText } = render(<StatisticScreen route={napRoute} />);
-      expect(getByText('Number of Naps')).toBeTruthy();
+      const { getByText } = render(<StatisticScreen events={napEvents} />);
+      expect(getByText('Number of Naps per Day')).toBeTruthy();
+      expect(getByText('1.3')).toBeTruthy();
+    });
+    it('calculates average bed time and wake up time correctly', () => {
+      const { getByText } = render(
+        <StatisticScreen events={nightSleepEvents} />
+      );
+      expect(getByText('Bed Time')).toBeTruthy();
+      expect(getByText('10:00 PM')).toBeTruthy();
+    });
+    it('calculates average wake up time correctly', () => {
+      const { getByText } = render(
+        <StatisticScreen events={nightSleepEvents} />
+      );
+      expect(getByText('Wake Up Time')).toBeTruthy();
+      expect(getByText('6:30 AM')).toBeTruthy();
+    });
+    it('calculates average number of wake up window', () => {
+      const { getByText } = render(
+        <StatisticScreen events={nightSleepEvents} />
+      );
+      expect(getByText('Wakings per Night')).toBeTruthy();
       expect(getByText('1')).toBeTruthy();
+    });
+    it('calculates average number of feeds correctly', () => {
+      const { getByText } = render(<StatisticScreen events={feedEvents} />);
+      expect(getByText('Number of Feed per Day')).toBeTruthy();
+      expect(getByText('2.5')).toBeTruthy();
+    });
+    it('calculates average number of feeds correctly', () => {
+      const { getByText } = render(<StatisticScreen events={feedEvents} />);
+      expect(getByText('Number of Feed at Night')).toBeTruthy();
+      expect(getByText('0.5')).toBeTruthy();
+    });
+  });
+
+  describe('chart', () => {
+    it('nap chart renders correctly', () => {
+      const { getByText } = render(<StatisticScreen events={napEvents} />);
+      expect(getByText('Number of Naps per Day')).toBeTruthy();
+      fireEvent.press(getByText('Week'));
+      expect(getByText('Mon')).toBeTruthy();
+      fireEvent.press(getByText('Month'));
+      expect(getByText('01')).toBeTruthy();
+    });
+    it('night sleep chart renders correctly', () => {
+      const { getByText } = render(
+        <StatisticScreen events={nightSleepEvents} />
+      );
+      expect(getByText('Number of Wakings per Night')).toBeTruthy();
+      fireEvent.press(getByText('Week'));
+      expect(getByText('Mon')).toBeTruthy();
+      fireEvent.press(getByText('Month'));
+      expect(getByText('01')).toBeTruthy();
     });
   });
 });
--- mobile/__tests__/screens/StatisticsScreen.test.tsx
+++ mobile/__tests__/screens/StatisticsScreen.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import type { ReactNode } from 'react';
+import { render } from '@testing-library/react-native';
 import StatisticsScreen from '../../src/screens/StatisticsScreen';
-import { fetchEvents } from '../../src/utils/db';
 
 // Mock the fetchEvents function
 jest.mock('../../src/utils/db', () => ({
@@ -42,13 +42,30 @@ jest.mock('../../src/utils/db', () => ({
   ])
 }));
 
-// Mock navigation function
-const mockNavigate = jest.fn();
-jest.mock('@react-navigation/native', () => ({
-  ...jest.requireActual('@react-navigation/native'),
-  useNavigation: () => ({
-    navigate: mockNavigate
-  })
+jest.mock('../../src/components/misc/TransitionHeader', () => ({
+  TransitionHeader: ({
+    children,
+    onBack,
+    onForward
+  }: {
+    children: ReactNode;
+    onBack: () => void;
+    onForward: () => void;
+  }) => (
+    <div>
+      <button
+        data-testid="date-back-button"
+        onClick={onBack}>
+        Back
+      </button>
+      <span>{children}</span>
+      <button
+        data-testid="date-forward-button"
+        onClick={onForward}>
+        Forward
+      </button>
+    </div>
+  )
 }));
 
 describe('<StatisticsScreen />', () => {
@@ -56,79 +73,8 @@ describe('<StatisticsScreen />', () => {
     jest.clearAllMocks();
   });
 
-  test('renders correctly', async () => {
-    const { getByText, findByText } = render(<StatisticsScreen />);
-
-    expect(getByText('Night Sleep')).toBeTruthy();
-    expect(getByText('Nap')).toBeTruthy();
-    expect(getByText('Feed')).toBeTruthy();
-
-    const nightSleepButton = getByText('Night Sleep');
-    fireEvent.press(nightSleepButton);
-    await findByText('Night Sleep');
-  });
-
-  test('navigation works correctly', async () => {
+  it('renders with correct title and buttons', async () => {
     const { getByText } = render(<StatisticsScreen />);
-    await waitFor(() => {
-      expect(fetchEvents).toHaveBeenCalled();
-    });
-    const nightSleepButton = getByText('Night Sleep');
-    const napButton = getByText('Nap');
-    const feedButton = getByText('Feed');
-
-    fireEvent.press(nightSleepButton);
-    expect(mockNavigate).toHaveBeenCalledWith('StatisticScreen', {
-      eventType: 'night_sleep',
-      events: [
-        {
-          eventId: '1',
-          ownerId: '2',
-          startTime: new Date('2021-01-01T00:00:00Z'),
-          endTime: new Date('2021-01-01T08:00:00Z'),
-          type: 'night_sleep'
-        }
-      ]
-    });
-
-    fireEvent.press(napButton);
-    expect(mockNavigate).toHaveBeenCalledWith('StatisticScreen', {
-      eventType: 'nap',
-      events: [
-        {
-          eventId: '2',
-          ownerId: '2',
-          startTime: new Date('2021-01-02T00:00:00Z'),
-          endTime: new Date('2021-01-02T08:00:00Z'),
-          type: 'nap'
-        },
-        {
-          eventId: '5',
-          ownerId: '2',
-          startTime: new Date('2021-01-03T09:00:00Z'),
-          endTime: new Date('2021-01-03T10:00:00Z'),
-          type: 'nap'
-        }
-      ]
-    });
-
-    fireEvent.press(feedButton);
-    expect(mockNavigate).toHaveBeenCalledWith('StatisticScreen', {
-      eventType: 'feed',
-      events: [
-        {
-          eventId: '3',
-          ownerId: '2',
-          startTime: new Date('2021-01-02T03:00:00Z'),
-          type: 'feed'
-        },
-        {
-          eventId: '4',
-          ownerId: '2',
-          startTime: new Date('2021-01-02T09:00:00Z'),
-          type: 'feed'
-        }
-      ]
-    });
+    expect(getByText('Night Sleep Statistics')).toBeTruthy();
   });
 });
