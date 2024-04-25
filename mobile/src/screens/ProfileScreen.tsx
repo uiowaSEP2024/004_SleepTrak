345f4dd2ef12bcae9adb5b2c6b7fba85368e25d0
SergioMEV <sergiomarteloe@gmail.com>
2024-04-25 14:28:48 -0500

    refactor: refactors profile screen and fixes bugs

--- mobile/src/screens/ProfileScreen.tsx
+++ mobile/src/screens/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import {
   View,
   StyleSheet,
@@ -7,87 +7,73 @@ import {
   TextInput
 } from 'react-native';
 import { Divider, Text } from 'react-native-paper';
-import { colors } from '../../assets/colors';
 import { useFocusEffect } from '@react-navigation/native';
+import RNPickerSelect from 'react-native-picker-select';
+
+import { colors } from '../../assets/colors';
 import {
   fetchUserData,
   fetchOnboardingAnswers,
   fetchOnboardingQuestions,
   updateOnboardingAnswer
 } from '../utils/db';
-import RNPickerSelect from 'react-native-picker-select';
 
-const ANSWERS: Record<string, string> = {};
 const HIDDEN_QUESTIONS = ['4', '9', '12', '1', '2', '3'];
 
-const capitalize = (s: string) => {
-  return s.charAt(0).toLocaleUpperCase() + s.slice(1).toLocaleLowerCase();
-};
+const capitalize = (s: string) =>
+  s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
 
 const Item = ({
   title,
   value,
-  answerId
+  answerId,
+  setAnswers
 }: {
   title: string;
   value: string;
   answerId: string;
+  setAnswers: (answerId: string, text: string) => void;
 }) => {
-  const [text, setText] = React.useState(value);
+  const [text, setText] = useState(value);
 
   const onChangeText = (text: string) => {
     setText(text);
-    ANSWERS[answerId] = text;
+    setAnswers(answerId, text);
   };
 
   return (
-    <View>
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemTitle}>{title}</Text>
-        <TextInput
-          value={text}
-          onChangeText={onChangeText}
-          style={styles.itemValue}
-        />
-        <Text
-          style={{
-            fontSize: 12,
-            color: colors.textGray,
-            alignSelf: 'flex-end'
-          }}>
-          Tap to edit
-        </Text>
-      </View>
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemTitle}>{title}</Text>
+      <TextInput
+        value={text}
+        onChangeText={onChangeText}
+        style={styles.itemValue}
+      />
+      <Text style={styles.editText}>Tap to edit</Text>
     </View>
   );
 };
 
 const ProfileScreen: React.FC = () => {
-  const [user, setUser] = React.useState<{
-    userId: any;
-    coachId: any;
-    role: any;
-    email: any;
-    first_name: any;
-    last_name: any;
-    babies: any;
-    events: any;
-    medicines: any | undefined;
-  }>();
-  const [onboardingAnswers, setOnboardingAnswers] = React.useState<any>();
-  const [onboardingQuestions, setOnboardingQuestions] = React.useState<any>();
-  const [currentBaby, setCurrentBaby] = React.useState<any>(null);
+  const [user, setUser] = useState<any>();
+  const [onboardingAnswers, setOnboardingAnswers] = useState<any>();
+  const [onboardingQuestions, setOnboardingQuestions] = useState<any>();
+  const [currentBaby, setCurrentBaby] = useState<any>(null);
+  const [answers, setAnswers] = useState<Record<string, string>>({});
+
+  const updateAnswers = (answerId: string, text: string) => {
+    setAnswers((prevAnswers) => ({ ...prevAnswers, [answerId]: text }));
+  };
 
   useFocusEffect(
-    React.useCallback(() => {
+    useCallback(() => {
       const fetchUser = async () => {
         const userData = await fetchUserData();
         setCurrentBaby(userData.babies[0].babyId);
         setUser(userData);
       };
       const fetchQuestions = async () => {
-        const onboardingQuestions = await fetchOnboardingQuestions();
-        setOnboardingQuestions(onboardingQuestions);
+        setOnboardingQuestions(await fetchOnboardingQuestions());
       };
 
       void fetchUser();
@@ -95,96 +81,86 @@ const ProfileScreen: React.FC = () => {
     }, [])
   );
 
-  useFocusEffect(
-    React.useCallback(() => {
-      const fetchAnswers = async () => {
-        if (!currentBaby) return;
+  useEffect(() => {
+    const fetchAnswers = async () => {
+      if (!currentBaby) return;
 
-        let onboardingAnswers = await fetchOnboardingAnswers(currentBaby);
-        onboardingAnswers = onboardingAnswers.filter(
-          (answer: any) => !HIDDEN_QUESTIONS.includes(answer.questionId)
-        );
-        setOnboardingAnswers(
-          onboardingAnswers.sort(
-            (a: any, b: any) => parseInt(a.questionId) - parseInt(b.questionId)
-          )
-        );
-      };
+      let onboardingAnswers = await fetchOnboardingAnswers(currentBaby);
+      onboardingAnswers = onboardingAnswers.filter(
+        (answer: any) => !HIDDEN_QUESTIONS.includes(answer.questionId)
+      );
+      setOnboardingAnswers(
+        onboardingAnswers.sort(
+          (a: any, b: any) => parseInt(a.questionId) - parseInt(b.questionId)
+        )
+      );
 
-      void fetchAnswers();
-    }, [currentBaby])
-  );
+      setAnswers({});
+    };
+
+    void fetchAnswers();
+  }, [currentBaby]);
 
   const onSave = () => {
     const updateAnswers = async () => {
-      const tempAnswers = ANSWERS;
-      console.log(tempAnswers);
-      for (const [answerId, answer] of Object.entries(tempAnswers)) {
-        console.log(answerId, answer);
+      for (const [answerId, answer] of Object.entries(answers)) {
         await updateOnboardingAnswer(answer, answerId);
-        delete ANSWERS.answerId;
       }
+      setAnswers({});
     };
     void updateAnswers();
   };
 
-  return user ? (
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Looking for data...</Text>
+      </View>
+    );
+  }
+
+  return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>
-        {' '}
-        {capitalize(user.first_name)} {capitalize(user.last_name)}
-      </Text>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text
-          style={{ ...pickerSelectStyles.inputIOS, color: colors.textGray }}>
-          Current Baby:
-        </Text>
+      <Text style={styles.title}>{`${capitalize(user.first_name)} ${capitalize(
+        user.last_name
+      )}`}</Text>
+      <View style={styles.row}>
+        <Text style={pickerSelectStyles.inputIOS}>Current Baby:</Text>
         <RNPickerSelect
           value={currentBaby}
           onValueChange={(value) => {
             setCurrentBaby(value);
           }}
-          items={user.babies.map((baby: any) => {
-            return {
-              label: baby.name,
-              value: baby.babyId
-            };
-          })}
+          items={user.babies.map((baby: any) => ({
+            label: baby.name,
+            value: baby.babyId
+          }))}
           style={pickerSelectStyles}
         />
       </View>
-      <Divider style={{ marginVertical: 8 }} />
+      <Divider style={styles.divider} />
       <Text style={styles.sectionTitle}>Onboarding Questions</Text>
       <ScrollView contentInset={{ top: 0, bottom: 250, left: 0, right: 0 }}>
-        {onboardingAnswers
-          ? onboardingAnswers.map((answer: any) => {
-              return (
-                <Item
-                  key={answer.questionId}
-                  answerId={answer.answerId}
-                  title={
-                    onboardingQuestions.find(
-                      (question: any) =>
-                        question.questionId === answer.questionId
-                    )?.description || ''
-                  }
-                  value={answer.answer_text}
-                />
-              );
-            })
-          : null}
+        {onboardingAnswers?.map((answer: any) => (
+          <Item
+            key={answer.questionId}
+            answerId={answer.answerId}
+            title={
+              onboardingQuestions.find(
+                (question: any) => question.questionId === answer.questionId
+              )?.description || ''
+            }
+            value={answer.answer_text}
+            setAnswers={updateAnswers}
+          />
+        ))}
       </ScrollView>
-
       <TouchableOpacity
         onPress={onSave}
         style={styles.editButton}>
         <Text style={styles.editButtonText}>Save</Text>
       </TouchableOpacity>
     </ScrollView>
-  ) : (
-    <View style={styles.container}>
-      <Text style={styles.title}>Looking for data...</Text>
-    </View>
   );
 };
 
@@ -201,7 +177,6 @@ const styles = StyleSheet.create({
     fontSize: 32,
     letterSpacing: 2,
     marginBottom: '2%'
-    // marginTop: '5%'
   },
   sectionTitle: {
     color: colors.textGray,
@@ -217,10 +192,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginVertical: 8
   },
-  itemTitle: {
-    fontSize: 18,
-    color: colors.crimsonRed
-  },
+  itemTitle: { fontSize: 18, color: colors.crimsonRed },
   itemValue: {
     fontSize: 20,
     color: colors.textGray,
@@ -229,6 +201,7 @@ const styles = StyleSheet.create({
     padding: 12,
     marginVertical: 4
   },
+  editText: { fontSize: 12, color: colors.textGray, alignSelf: 'flex-end' },
   editButton: {
     position: 'absolute',
     alignSelf: 'center',
@@ -246,7 +219,9 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     fontWeight: 'bold',
     letterSpacing: 2
-  }
+  },
+  row: { flexDirection: 'row', justifyContent: 'space-between' },
+  divider: { marginVertical: 8 }
 });
 
 const pickerSelectStyles = StyleSheet.create({
@@ -268,4 +243,5 @@ const pickerSelectStyles = StyleSheet.create({
     fontWeight: '500'
   }
 });
+
 export default ProfileScreen;
