3d31d1fb397b9d3dfc1211f3a5a3592a0cba131f
Haruko Okada <hokada@uiowa.edu>
2024-05-02 18:54:27 -0500

    test: test url link button

--- mobile/__tests__/screens/SettingScreen.test.tsx
+++ mobile/__tests__/screens/SettingScreen.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Linking } from 'react-native';
 import { act, fireEvent, render } from '@testing-library/react-native';
 import Setting from '../../src/screens/SettingScreen';
 import { NavigationContainer } from '@react-navigation/native';
@@ -15,6 +15,10 @@ jest.mock('../../src/utils/db', () => ({
 }));
 
 describe('Setting', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders without crashing', () => {
     render(
       <NavigationContainer>
@@ -49,5 +53,35 @@ describe('Setting', () => {
     await act(async () => {
       expect(getByText('Files Screen')).toBeTruthy();
     });
+  });
+
+  it('should call onPress when pressed', async () => {
+    jest.mock('react-native', () => {
+      const rn = jest.requireActual('react-native');
+      rn.Linking = {
+        ...rn.Linking,
+        openURL: jest.fn(),
+        canOpenURL: jest.fn(async () => await Promise.resolve(true))
+      };
+      return rn;
+    });
+    const url =
+      'https://team4wiki.notion.site/User-Manual-for-Mobile-434e715c87bb4194b10d0fd9d812a47d?pvs=74';
+    const { findByText } = render(
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Setting"
+            component={Setting}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+    const button = await findByText('User Manual');
+    fireEvent.press(button);
+    await act(async () => {
+      await Promise.resolve();
+    });
+    expect((Linking.openURL as jest.Mock).mock.calls[0][0]).toBe(url);
   });
 });
