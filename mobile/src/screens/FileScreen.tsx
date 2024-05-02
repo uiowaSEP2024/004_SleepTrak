290e858bbd11b4ad3ceb44b0d6bf59699b08a766
SergioMEV <sergiomarteloe@gmail.com>
2024-05-02 12:06:42 -0500

    style: makes the setting and files screens better looking

--- mobile/src/screens/FileScreen.tsx
+++ mobile/src/screens/FileScreen.tsx
@@ -42,51 +42,9 @@ const styles = StyleSheet.create({
   title: {
     color: colors.textGray,
     alignSelf: 'flex-start',
-    fontSize: 32,
+    fontSize: 20,
     letterSpacing: 2,
     marginBottom: '5%'
-  },
-  itemContainer: {
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    width: '100%',
-    alignSelf: 'center',
-    marginVertical: 8
-  },
-  fileTitle: {
-    fontSize: 18,
-    color: colors.crimsonRed
-  },
-  itemValue: {
-    fontSize: 20,
-    color: colors.textGray,
-    backgroundColor: colors.veryLightPurple,
-    borderRadius: 8,
-    padding: 12,
-    marginVertical: 4
-  },
-  tapToEdit: {
-    fontSize: 12,
-    color: colors.textGray,
-    alignSelf: 'flex-end'
-  },
-  editButton: {
-    position: 'absolute',
-    alignSelf: 'center',
-    alignContent: 'center',
-    justifyContent: 'center',
-    bottom: '6%',
-    backgroundColor: colors.crimsonRed,
-    width: '70%',
-    height: '7%',
-    borderRadius: 32
-  },
-  editButtonText: {
-    fontSize: 20,
-    color: 'white',
-    alignSelf: 'center',
-    fontWeight: 'bold',
-    letterSpacing: 2
   },
   pdf: {
     flex: 1,
